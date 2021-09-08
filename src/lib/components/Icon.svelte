94392aa3c40ef0faff31a7385fd756e7ebea7f56
Sean Lynch <techniq35@gmail.com>
2021-09-08 17:15:14 -0400

    Add Icon component

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let size = '1.5em';
+	export let width = size;
+	export let height = size;
+	export let color = 'currentColor';
+	export let viewBox = '0 0 24 24';
+	export let path = '';
+</script>
+
+<svg {width} {height} {viewBox} class="inline-block {$$props.class}" style={$$props.style}>
+	<path d={path} fill={color} />
+</svg>
