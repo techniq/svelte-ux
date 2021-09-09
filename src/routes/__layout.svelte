1c2a956288f0cb07d1f2e3dba44167a38cd05ef0
Sean Lynch <techniq35@gmail.com>
2021-09-09 00:09:52 -0400

    Adjust doc styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,12 +2,17 @@
 	import 'prism-themes/themes/prism-vsc-dark-plus.css';
 </script>
 
-<div class="p-4">
-	<slot />
-</div>
+<slot />
 
 <style lang="postcss">
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
+
+	:global(h2) {
+		@apply text-lg font-semibold mt-8 mb-1 ml-2;
+	}
+	:global(h2:first-of-type) {
+		@apply mt-0;
+	}
 </style>
