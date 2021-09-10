324fd5a5327ef0e49a96fe88b45b8f8bdc72edad
Sean Lynch <techniq35@gmail.com>
2021-09-10 11:00:26 -0400

    [Icon] Remove color prop and require to be set by currentColor (which can be passed as class prop).

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -2,11 +2,10 @@
 	export let size = '1.5em';
 	export let width = size;
 	export let height = size;
-	export let color = 'currentColor';
 	export let viewBox = '0 0 24 24';
 	export let path = '';
 </script>
 
 <svg {width} {height} {viewBox} class="inline-block {$$props.class}" style={$$props.style}>
-	<path d={path} fill={color} />
+	<path d={path} fill="currentColor" />
 </svg>
