960094eaee0d47e5749624a25a726e8ee9224952
Sean Lynch <techniq35@gmail.com>
2021-09-11 09:43:42 -0400

    Extract Menu into separate component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,50 +1,13 @@
-<script>
+<script lang="ts">
 	import 'prism-themes/themes/prism-vsc-dark-plus.css';
 
 	import AppLayout from '$lib/components/AppLayout.svelte';
-	import MenuItem from '$lib/components/MenuItem.svelte';
+	import Menu from './_Menu.svelte';
 </script>
 
 <AppLayout>
 	<nav slot="nav" class="nav h-full">
-		<MenuItem text="Home" path="/" class="pl-6 py-2 mt-4" />
-
-		<div
-			class="py-2 pl-4 mt-4 text-sm text-gray-200 font-bold bg-black/20 border-t border-b border-white/10"
-		>
-			Components
-		</div>
-
-		<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">App</div>
-		<MenuItem text="AppBar" path="/docs/components/AppBar" class="pl-6 py-2" />
-		<MenuItem text="Breadcrumb" path="/docs/components/Breadcrumb" class="pl-6 py-2" />
-
-		<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">General</div>
-		<MenuItem text="Avatar" path="/docs/components/Avatar" class="pl-6 py-2" />
-		<MenuItem text="Button" path="/docs/components/Button" class="pl-6 py-2" />
-		<MenuItem text="Card" path="/docs/components/Card" class="pl-6 py-2" />
-		<MenuItem text="Icon" path="/docs/components/Icon" class="pl-6 py-2" />
-		<MenuItem text="Progress" path="/docs/components/Progress" class="pl-6 py-2" />
-		<MenuItem text="Tabs" path="/docs/components/Tabs" class="pl-6 py-2" />
-
-		<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Inputs</div>
-		<MenuItem text="Checkbox" path="/docs/components/Checkbox" class="pl-6 py-2" />
-		<MenuItem text="Field" path="/docs/components/Field" class="pl-6 py-2" />
-		<MenuItem text="Radio" path="/docs/components/Radio" class="pl-6 py-2" />
-		<MenuItem text="Switch" path="/docs/components/Switch" class="pl-6 py-2" />
-		<MenuItem text="TextField" path="/docs/components/TextField" class="pl-6 py-2" />
-
-		<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Layout</div>
-		<MenuItem text="Grid" path="/docs/components/Grid" class="pl-6 py-2" />
-		<MenuItem text="Stack" path="/docs/components/Stack" class="pl-6 py-2" />
-
-		<div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Modal</div>
-		<MenuItem text="Dialog" path="/docs/components/Dialog" class="pl-6 py-2" />
-		<MenuItem text="Drawer" path="/docs/components/Drawer" class="pl-6 py-2" />
-		<MenuItem text="Menu" path="/docs/components/Menu" class="pl-6 py-2" />
-		<MenuItem text="Notification" path="/docs/components/Notification" class="pl-6 py-2" />
-		<MenuItem text="Popover" path="/docs/components/Popover" class="pl-6 py-2" />
-		<MenuItem text="Tooltip" path="/docs/components/Tooltip" class="pl-6 py-2" />
+		<Menu />
 	</nav>
 
 	<slot />
