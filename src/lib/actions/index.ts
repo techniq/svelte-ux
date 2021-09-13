8e32652a09493f18f132fd4a2809ca2c0e78bb9e
Sean Lynch <techniq35@gmail.com>
2021-09-13 07:53:50 -0400

    Update all top-level exports

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,2 +1,6 @@
 export { default as cssVars } from './cssVars';
+export * from './input';
+export * from './layout';
+export { default as multi } from './multi';
 export { default as portal } from './portal';
+export * from './scroll';
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,2 +1,18 @@
-export * from './dom';
-export * from './routing';
+// alias exports to remove conflicts (and make imports less noisy from top-level)
+export * as array from './array';
+export * as date from './date';
+export * as dateDisplay from './dateDisplay';
+export * as dateRange from './dateRange';
+export * as dom from './dom';
+export * as duration from './duration';
+export * as json from './json';
+export * as logger from './logger';
+export * as object from './object';
+export * as promise from './promise';
+export * as rollup from './rollup';
+export * as routing from './routing';
+export * as serialize from './serialize';
+export * as styles from './styles';
+export * as transition from './transition';
+export * as typeGuards from './typeGuards';
+export * as typeHelpers from './typeHelpers';
