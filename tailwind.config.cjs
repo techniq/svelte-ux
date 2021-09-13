627bcc0b0e034b9744ca9908e61d0b0005474dc3
Sean Lynch <techniq35@gmail.com>
2021-09-12 20:07:42 -0400

    Add tailwind colors as CSS variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
 	mode: 'jit',
@@ -16,5 +17,27 @@ module.exports = {
 	variants: {
 		extend: {}
 	},
-	plugins: [require('tailwindcss-elevation')(['responsive'])]
+	plugins: [
+		require('tailwindcss-elevation')(['responsive']),
+
+		// Expose color palette as CSS variables (--color-xxx-yyy) - https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
+		plugin(function ({ addBase, theme }) {
+			function extractColorVars(colorObj, colorGroup = '') {
+				return Object.keys(colorObj).reduce((vars, colorKey) => {
+					const value = colorObj[colorKey];
+
+					const newVars =
+						typeof value === 'string'
+							? { [`--color${colorGroup}-${colorKey}`]: value }
+							: extractColorVars(value, `-${colorKey}`);
+
+					return { ...vars, ...newVars };
+				}, {});
+			}
+
+			addBase({
+				':root': extractColorVars(theme('colors'))
+			});
+		})
+	]
 };
