50d4230290bc941de3aaebd22dd98ff4817aea53
Sean Lynch <techniq35@gmail.com>
2021-09-12 20:09:32 -0400

    Update more text-secondary => text-black/50 and bg-black/4 => bg-black/5

--- src/lib/components/EmptyMessage.svelte
+++ src/lib/components/EmptyMessage.svelte
@@ -1,5 +1,5 @@
 <div
-	class="grid place-items-center h-16 bg-black/10 shadow-inner ring-1 ring-black/30 text-secondary text-xs border rounded tracking-wider"
+	class="grid place-items-center h-16 bg-black/10 shadow-inner ring-1 ring-black/30 text-black/50 text-xs border rounded tracking-wider"
 >
 	<slot />
 </div>
