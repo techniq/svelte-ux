d9412a8b540f958452aa834d4a4beef45630f3ea
Sean Lynch <techniq35@gmail.com>
2021-09-13 14:58:46 -0400

    Fix top-level exports of stores

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,8 +1,8 @@
-export * from './debounceStore';
-export * from './fetchStore';
-export * from './graphStore';
-export * from './immerStore';
-export * from './localStore';
-export * from './matchMedia';
-export * from './paginationStore';
+export { default as debounceStore } from './debounceStore';
+export { default as fetchStore } from './fetchStore';
+export { default as graphStore } from './graphStore';
+export { default as immerStore } from './immerStore';
+export { default as localStore } from './localStore';
+export { default as matchMedia } from './matchMedia';
+export { default as paginationStore } from './paginationStore';
 export * from './queryParamsStore';
