e16108529a33e99b417adf3105f56f59ce97403c
Sean Lynch <techniq35@gmail.com>
2021-09-14 07:27:00 -0400

    Export initGraphClient from top-level exports

--- src/lib/stores/graphStore.ts
+++ src/lib/stores/graphStore.ts
@@ -8,79 +8,79 @@ import fetchStore from './fetchStore';
 import type { FetchConfig } from './fetchStore';
 
 type ClientConfig = {
-  url: string;
-  options: () => RequestInit;
+	url: string;
+	options: () => RequestInit;
 };
 
 export type GraphQLError = {
-  message: string;
-  extensions?: any;
+	message: string;
+	extensions?: any;
 };
 
 const CONTEXT_KEY = {};
 
-export function initClient(config: ClientConfig) {
-  setContext(CONTEXT_KEY, config);
+export function initGraphClient(config: ClientConfig) {
+	setContext(CONTEXT_KEY, config);
 }
 
 export type QueryConfig = {
-  query?: string | DocumentNode;
-  variables?: any;
-  config?: FetchConfig;
+	query?: string | DocumentNode;
+	variables?: any;
+	config?: FetchConfig;
 };
 
-export default function queryStore(baseQueryConfig?: QueryConfig) {
-  const client = getContext<ClientConfig>(CONTEXT_KEY);
-  const { subscribe, fetch, refresh, clear, fetchConfig } = fetchStore();
+export default function graphStore(baseQueryConfig?: QueryConfig) {
+	const client = getContext<ClientConfig>(CONTEXT_KEY);
+	const { subscribe, fetch, refresh, clear, fetchConfig } = fetchStore();
 
-  // Save for building derived requests (ex. exports)
-  const queryConfigStore = writable(baseQueryConfig);
+	// Save for building derived requests (ex. exports)
+	const queryConfigStore = writable(baseQueryConfig);
 
-  function doFetch(queryConfig?: QueryConfig) {
-    const mergedQueryConfig = merge(baseQueryConfig, queryConfig, {});
-    queryConfigStore.set(mergedQueryConfig);
+	function doFetch(queryConfig?: QueryConfig) {
+		const mergedQueryConfig = merge(baseQueryConfig, queryConfig, {});
+		queryConfigStore.set(mergedQueryConfig);
 
-    const { query, variables, config } = mergedQueryConfig;
+		const { query, variables, config } = mergedQueryConfig;
 
-    // https://github.com/apollographql/graphql-tag/issues/144
-    const queryAsString = typeof query === 'object' ? print(query) : query;
+		// https://github.com/apollographql/graphql-tag/issues/144
+		const queryAsString = typeof query === 'object' ? print(query) : query;
 
-    const options: RequestInit = merge(
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query: queryAsString, variables }),
-      },
-      client.options?.(),
-      config?.options?.()
-    );
+		const options: RequestInit = merge(
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ query: queryAsString, variables })
+			},
+			client.options?.(),
+			config?.options?.()
+		);
 
-    const mergedConfig: FetchConfig = merge(
-      {
-        as: async (res) => {
-          const body = await res.json();
-          if (body.errors) {
-            throw body.errors;
-          } else {
-            return body.data;
-          }
-        },
-        options: () => options,
-      } as FetchConfig,
-      config
-    );
+		const mergedConfig: FetchConfig = merge(
+			{
+				as: async (res) => {
+					const body = await res.json();
+					if (body.errors) {
+						throw body.errors;
+					} else {
+						return body.data;
+					}
+				},
+				options: () => options
+			} as FetchConfig,
+			config
+		);
 
-    return fetch(client.url, mergedConfig);
-  }
+		return fetch(client.url, mergedConfig);
+	}
 
-  return {
-    subscribe,
-    fetch: doFetch,
-    refresh,
-    clear,
-    queryConfig: queryConfigStore,
-    fetchConfig,
-  };
+	return {
+		subscribe,
+		fetch: doFetch,
+		refresh,
+		clear,
+		queryConfig: queryConfigStore,
+		fetchConfig
+	};
 }
--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,6 +1,6 @@
 export { default as debounceStore } from './debounceStore';
 export { default as fetchStore } from './fetchStore';
-export { default as graphStore } from './graphStore';
+export { default as graphStore, initGraphClient } from './graphStore';
 export { default as immerStore } from './immerStore';
 export { default as localStore } from './localStore';
 export { default as matchMedia } from './matchMedia';
