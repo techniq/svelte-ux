c37178c8d06f5ea395f48c389c8f88277a2e2ea5
Sean Lynch <techniq35@gmail.com>
2021-09-14 15:42:19 -0400

    [EmptyMessage] Support passing class prop

--- src/lib/components/EmptyMessage.svelte
+++ src/lib/components/EmptyMessage.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+	import clsx from 'clsx';
+</script>
+
 <div
-	class="grid place-items-center h-16 bg-black/10 shadow-inner ring-1 ring-black/30 text-black/50 text-xs border rounded tracking-wider"
+	class={clsx(
+		'grid place-items-center',
+		'h-16 bg-black/10 shadow-inner ring-1 ring-black/30',
+		'text-black/50 text-xs tracking-wider',
+		'border rounded',
+		$$props.class
+	)}
 >
 	<slot />
 </div>
