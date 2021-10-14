e190086154d562c2d68f6b4ca8a841f36e775a51
Sean Lynch <techniq35@gmail.com>
2021-10-13 22:13:28 -0400

    [NavItem] Add show prop to show/hide (based on access, etc)

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -9,44 +9,47 @@
   export let path: string;
   export let text: string;
   export let icon: string | null = null;
+  export let show = true;
 
   $: isPathActive = $isActive(path);
 </script>
 
-<a
-  href={url(path)}
-  class={clsx(
-    'flex items-center text-sm p-1 hover:text-white hover:bg-gray-300/10 relative',
-    isPathActive ? ['text-sky-400', 'bg-gray-500/10'] : 'text-gray-400',
-    $$props.class
-  )}
-  use:scrollIntoView={{
-    condition: (node) => {
-      if (isPathActive) {
-        const nodeTop = node.getBoundingClientRect().top;
-        const scrollParent = getScrollParent(node);
-        const scrollParentHeight = scrollParent.offsetHeight;
-        const threshold = 0;
-        const isVisible = scrollParentHeight - nodeTop < threshold;
-        return isVisible;
-      } else {
-        return false;
-      }
-    },
-    delay: 500,
-  }}
->
-  {#if isPathActive}
-    <div class="absolute left-0 top-0 w-1 h-full bg-light-blue-300" />
-  {/if}
+{#if show}
+  <a
+    href={url(path)}
+    class={clsx(
+      'flex items-center text-sm p-1 hover:text-white hover:bg-gray-300/10 relative',
+      isPathActive ? ['text-sky-400', 'bg-gray-500/10'] : 'text-gray-400',
+      $$props.class
+    )}
+    use:scrollIntoView={{
+      condition: (node) => {
+        if (isPathActive) {
+          const nodeTop = node.getBoundingClientRect().top;
+          const scrollParent = getScrollParent(node);
+          const scrollParentHeight = scrollParent.offsetHeight;
+          const threshold = 0;
+          const isVisible = scrollParentHeight - nodeTop < threshold;
+          return isVisible;
+        } else {
+          return false;
+        }
+      },
+      delay: 500,
+    }}
+  >
+    {#if isPathActive}
+      <div class="absolute left-0 top-0 w-1 h-full bg-light-blue-300" />
+    {/if}
 
-  {#if $$slots.avatar}
-    <slot name="avatar" />
-  {/if}
+    {#if $$slots.avatar}
+      <slot name="avatar" />
+    {/if}
 
-  {#if icon}
-    <Icon path={icon} class="mr-3" />
-  {/if}
+    {#if icon}
+      <Icon path={icon} class="mr-3" />
+    {/if}
 
-  {text}
-</a>
+    {text}
+  </a>
+{/if}
