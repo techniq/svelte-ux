87ab39cddb9387ef572e030b571e8d66c2a1b3a8
Sean Lynch <techniq35@gmail.com>
2021-11-08 16:07:42 -0500

    Remove commented out routing util implementations (unused)

--- src/lib/utils/routing.ts
+++ src/lib/utils/routing.ts
@@ -3,61 +3,6 @@ import { derived, get } from 'svelte/store';
 import { page as pageStore } from '$app/stores';
 // import { goto as gotoApp } from '$app/navigation';
 
-/*
-export const url = {
-  subscribe(listener) {
-    return derived(
-      page,
-      ($page) =>
-        function url(path: string) {
-          console.log({ $page, path });
-
-          if (path.match(/^\.\.?\//)) {
-            console.group('relative path');
-            // Relative path (starts wtih `./` or `../`)
-            let [, breadcrumbs, relativePath] = path.match(/^([\.\/]+)(.*)/);
-            let dir = $page.path.replace(/\/$/, '');
-            const traverse = breadcrumbs.match(/\.\.\//g) || [];
-            // if this is a page, we want to traverse one step back to its folder
-            // if (component.isPage) traverse.push(null)
-            traverse.forEach(() => (dir = dir.replace(/\/[^\/]+\/?$/, '')));
-            path = `${dir}/${relativePath}`.replace(/\/$/, '');
-            path = path || '/'; // empty means root
-            console.groupEnd();
-          } else if (path.match(/^\//)) {
-            // Absolute path (starts with `/`)
-            console.log('absoute path');
-            return path;
-          } else {
-            // NAMED PATH
-            console.log('named path');
-            // const matchingRoute = $routes.find(route => route.meta.name === path)
-            // if (matchingRoute) path = matchingRoute.shortPath
-            return path;
-          }
-
-          console.log({ path });
-          return path;
-        }
-    ).subscribe(listener);
-  },
-};
-
-export const goto = {
-  subscribe(listener) {
-    return derived(
-      [url],
-      ([$url]) =>
-        function goto(path: string) {
-          const newPath = $url(path);
-          debugger;
-          return gotoApp(newPath);
-        }
-    ).subscribe(listener);
-  },
-};
-*/
-
 // See: routify's helpers: https://github.com/roxiness/routify/blob/9a1b7f5f8fc950a344cf20f7cbaa760593ded8fb/runtime/helpers.js#L244-L268
 export function url(path: string, page?: any) {
   const pageValue = page ?? get(pageStore);
