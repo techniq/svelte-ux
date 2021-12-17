2cd6c2dcf8ea1bbcb220497afaf89cae5b30bbe5
Sean Lynch <techniq35@gmail.com>
2021-12-17 00:10:35 -0500

    [pannable] Support stepping by pixels or percent of parent widdth, and support ignoring axis changes (x/y)

--- src/lib/actions/mouse.ts
+++ src/lib/actions/mouse.ts
@@ -31,17 +31,30 @@ export function longpress(node: HTMLElement, duration: number) {
 /**
  * Track mouse position changes from mouse down on node to mouse up
  */
-export function pannable(node: HTMLElement): SvelteActionReturnType {
-  let x = 0;
-  let y = 0;
+type PannableOptions = {
+  /**
+   * Number of pixels to step
+   */
+  step?: number;
+
+  /**
+   * Percentage of parent element's pixels to step
+   */
+  stepPercent?: number;
+
+  axis?: 'x' | 'y' | 'xy';
+};
+export function pannable(node: HTMLElement, options: PannableOptions = {}): SvelteActionReturnType {
+  let lastX = 0;
+  let lastY = 0;
 
   function handleMouseDown(event: MouseEvent) {
-    x = event.clientX;
-    y = event.clientY;
+    lastX = event.clientX;
+    lastY = event.clientY;
 
     node.dispatchEvent(
       new CustomEvent('panstart', {
-        detail: { x, y },
+        detail: { x: lastX, y: lastY },
       })
     );
 
@@ -50,25 +63,66 @@ export function pannable(node: HTMLElement): SvelteActionReturnType {
   }
 
   function handleMouseMove(event: MouseEvent) {
-    const dx = event.clientX - x;
-    const dy = event.clientY - y;
-    x = event.clientX;
-    y = event.clientY;
+    let dx = event.clientX - lastX;
+    let dy = event.clientY - lastY;
 
-    node.dispatchEvent(
-      new CustomEvent('panmove', {
-        detail: { x, y, dx, dy },
-      })
-    );
+    const xEnabled = options?.axis?.includes('x') ?? true;
+    const yEnabled = options?.axis?.includes('y') ?? true;
+
+    if (options.step) {
+      if (Math.abs(dx) >= options.step) {
+        dx = dx - (dx % options.step);
+        lastX = event.clientX;
+      } else {
+        dx = 0;
+      }
+
+      if (Math.abs(dy) >= options.step) {
+        dy = dy - (dy % options.step);
+        lastY = event.clientY;
+      } else {
+        dy = 0;
+      }
+    } else if (options.stepPercent) {
+      const parentWidth = node.parentElement.offsetWidth;
+      const parentHeight = node.parentElement.offsetHeight;
+
+      if (Math.abs(dx / parentWidth) >= options.stepPercent) {
+        dx = dx - (dx % (parentWidth * options.stepPercent));
+        lastX = event.clientX;
+      } else {
+        dx = 0;
+      }
+
+      if (Math.abs(dy / parentHeight) >= options.stepPercent) {
+        dy = dy - (dy % (parentHeight * options.stepPercent));
+        lastY = event.clientY;
+      } else {
+        dy = 0;
+      }
+    } else {
+      lastX = event.clientX;
+      lastY = event.clientY;
+    }
+
+    if ((xEnabled && dx) || (yEnabled && dy)) {
+      node.dispatchEvent(
+        new CustomEvent('panmove', {
+          detail: { x: lastX, y: lastX, dx: xEnabled ? dx : 0, dy: yEnabled ? dy : 0 },
+        })
+      );
+    } else {
+      // Not enough change
+    }
   }
 
   function handleMouseUp(event: MouseEvent) {
-    x = event.clientX;
-    y = event.clientY;
+    lastX = event.clientX;
+    lastY = event.clientY;
 
     node.dispatchEvent(
       new CustomEvent('panend', {
-        detail: { x, y },
+        detail: { x: lastX, y: lastY },
       })
     );
 
--- src/routes/docs/actions/mouse.md
+++ src/routes/docs/actions/mouse.md
@@ -59,3 +59,82 @@
     />
   </div>
 </Preview>
+
+### With pixel steps / snapping
+
+<Preview>
+  <div class="h-40">
+    <div class="w-10 h-10 bg-red-500 rounded cursor-move"
+      use:pannable={{ step: 25 }}
+      on:panstart={() => {
+        coords.stiffness = 1;
+        coords.damping = 1;
+      }}
+      on:panmove={(e) => {
+        $coords.x += e.detail.dx;
+        $coords.y += e.detail.dy;
+      }}
+      on:panend={() => {
+        coords.stiffness = 0.2;
+        coords.damping = 0.4;
+        coords.set({ x: 0, y: 0 });
+      }}
+      style="transform:
+        translate({$coords.x}px,{$coords.y}px)
+        rotate({$coords.x * 0.2}deg)"
+    />
+  </div>
+</Preview>
+
+### With percentage of parent steps / snapping
+
+<Preview>
+  <div class="h-40">
+    <div class="w-10 h-10 bg-red-500 rounded cursor-move"
+      use:pannable={{ stepPercent: .10 }}
+      on:panstart={() => {
+        coords.stiffness = 1;
+        coords.damping = 1;
+      }}
+      on:panmove={(e) => {
+        $coords.x += e.detail.dx;
+        $coords.y += e.detail.dy;
+      }}
+      on:panend={() => {
+        coords.stiffness = 0.2;
+        coords.damping = 0.4;
+        coords.set({ x: 0, y: 0 });
+      }}
+      style="transform:
+        translate({$coords.x}px,{$coords.y}px)
+        rotate({$coords.x * 0.2}deg)"
+    />
+  </div>
+</Preview>
+
+### x-axis only
+
+<Preview>
+  <div class="h-40">
+    <div class="w-10 h-10 bg-red-500 rounded cursor-move"
+      use:pannable={{ axis: 'x' }}
+      on:panstart={() => {
+        coords.stiffness = 1;
+        coords.damping = 1;
+      }}
+      on:panmove={(e) => {
+        $coords.x += e.detail.dx;
+        $coords.y += e.detail.dy;
+      }}
+      on:panend={() => {
+        coords.stiffness = 0.2;
+        coords.damping = 0.4;
+        coords.set({ x: 0, y: 0 });
+      }}
+      style="transform:
+        translate({$coords.x}px,{$coords.y}px)
+        rotate({$coords.x * 0.2}deg)
+      "
+    />
+  </div>
+</Preview>
