2c518cf1f0215669b92b3b8ab6ef875e10125249
Sean Lynch <techniq35@gmail.com>
2021-12-21 07:43:11 -0500

    Rename component doc files since now hosted on vercel instead of netlify

--- src/routes/_NavMenu.svelte
+++ src/routes/_NavMenu.svelte
@@ -11,66 +11,66 @@
 </div>
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">App</div>
-<NavItem text="AppBar" path="/docs/components/appbar" class="pl-6 py-2" />
-<NavItem text="Breadcrumb" path="/docs/components/breadcrumb" class="pl-6 py-2" />
-<NavItem text="Steps" path="/docs/components/steps" class="pl-6 py-2" />
+<NavItem text="AppBar" path="/docs/components/AppBar" class="pl-6 py-2" />
+<NavItem text="Breadcrumb" path="/docs/components/Breadcrumb" class="pl-6 py-2" />
+<NavItem text="Steps" path="/docs/components/Steps" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">General</div>
-<NavItem text="Avatar" path="/docs/components/avatar" class="pl-6 py-2" />
-<NavItem text="Badge" path="/docs/components/badge" class="pl-6 py-2" />
-<NavItem text="Button" path="/docs/components/button" class="pl-6 py-2" />
-<NavItem text="Card" path="/docs/components/card" class="pl-6 py-2" />
-<NavItem text="Collapse" path="/docs/components/collapse" class="pl-6 py-2" />
-<NavItem text="ExpansionPanel" path="/docs/components/expansionpanel" class="pl-6 py-2" />
-<NavItem text="Icon" path="/docs/components/icon" class="pl-6 py-2" />
-<NavItem text="ListItem" path="/docs/components/listitem" class="pl-6 py-2" />
-<NavItem text="Paginate" path="/docs/components/paginate" class="pl-6 py-2" />
-<NavItem text="Pagination" path="/docs/components/pagination" class="pl-6 py-2" />
-<NavItem text="Progress" path="/docs/components/progress" class="pl-6 py-2" />
-<NavItem text="Tabs" path="/docs/components/tabs" class="pl-6 py-2" />
+<NavItem text="Avatar" path="/docs/components/Avatar" class="pl-6 py-2" />
+<NavItem text="Badge" path="/docs/components/Badge" class="pl-6 py-2" />
+<NavItem text="Button" path="/docs/components/Button" class="pl-6 py-2" />
+<NavItem text="Card" path="/docs/components/Card" class="pl-6 py-2" />
+<NavItem text="Collapse" path="/docs/components/Collapse" class="pl-6 py-2" />
+<NavItem text="ExpansionPanel" path="/docs/components/ExpansionPanel" class="pl-6 py-2" />
+<NavItem text="Icon" path="/docs/components/Icon" class="pl-6 py-2" />
+<NavItem text="ListItem" path="/docs/components/ListItem" class="pl-6 py-2" />
+<NavItem text="Paginate" path="/docs/components/Paginate" class="pl-6 py-2" />
+<NavItem text="Pagination" path="/docs/components/Pagination" class="pl-6 py-2" />
+<NavItem text="Progress" path="/docs/components/Progress" class="pl-6 py-2" />
+<NavItem text="Tabs" path="/docs/components/Tabs" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Inputs</div>
-<NavItem text="Checkbox" path="/docs/components/checkbox" class="pl-6 py-2" />
-<NavItem text="Field" path="/docs/components/field" class="pl-6 py-2" />
-<NavItem text="Input" path="/docs/components/input" class="pl-6 py-2" />
-<NavItem text="Radio" path="/docs/components/radio" class="pl-6 py-2" />
+<NavItem text="Checkbox" path="/docs/components/Checkbox" class="pl-6 py-2" />
+<NavItem text="Field" path="/docs/components/Field" class="pl-6 py-2" />
+<NavItem text="Input" path="/docs/components/Input" class="pl-6 py-2" />
+<NavItem text="Radio" path="/docs/components/Radio" class="pl-6 py-2" />
 <NavItem text="RangeSlider" path="/docs/components/RangeSlider" class="pl-6 py-2" />
-<NavItem text="SelectField" path="/docs/components/selectfield" class="pl-6 py-2" />
-<NavItem text="Switch" path="/docs/components/switch" class="pl-6 py-2" />
-<NavItem text="TextField" path="/docs/components/textfield" class="pl-6 py-2" />
+<NavItem text="SelectField" path="/docs/components/SelectField" class="pl-6 py-2" />
+<NavItem text="Switch" path="/docs/components/Switch" class="pl-6 py-2" />
+<NavItem text="TextField" path="/docs/components/TextField" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Layout</div>
-<NavItem text="Grid" path="/docs/components/grid" class="pl-6 py-2" />
-<NavItem text="Stack" path="/docs/components/stack" class="pl-6 py-2" />
+<NavItem text="Grid" path="/docs/components/Grid" class="pl-6 py-2" />
+<NavItem text="Stack" path="/docs/components/Stack" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Modal</div>
-<NavItem text="Dialog" path="/docs/components/dialog" class="pl-6 py-2" />
-<NavItem text="Drawer" path="/docs/components/drawer" class="pl-6 py-2" />
-<NavItem text="Menu" path="/docs/components/menu" class="pl-6 py-2" />
-<NavItem text="Notification" path="/docs/components/notification" class="pl-6 py-2" />
-<NavItem text="Popover" path="/docs/components/popover" class="pl-6 py-2" />
-<NavItem text="Tooltip" path="/docs/components/tooltip" class="pl-6 py-2" />
+<NavItem text="Dialog" path="/docs/components/Dialog" class="pl-6 py-2" />
+<NavItem text="Drawer" path="/docs/components/Drawer" class="pl-6 py-2" />
+<NavItem text="Menu" path="/docs/components/Menu" class="pl-6 py-2" />
+<NavItem text="Notification" path="/docs/components/Notification" class="pl-6 py-2" />
+<NavItem text="Popover" path="/docs/components/Popover" class="pl-6 py-2" />
+<NavItem text="Tooltip" path="/docs/components/Tooltip" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">State</div>
-<NavItem text="Form" path="/docs/components/form" class="pl-6 py-2" />
-<NavItem text="State" path="/docs/components/state" class="pl-6 py-2" />
-<NavItem text="Toggle" path="/docs/components/toggle" class="pl-6 py-2" />
+<NavItem text="Form" path="/docs/components/Form" class="pl-6 py-2" />
+<NavItem text="State" path="/docs/components/State" class="pl-6 py-2" />
+<NavItem text="Toggle" path="/docs/components/Toggle" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Table</div>
-<NavItem text="Table" path="/docs/components/table" class="pl-6 py-2" />
-<NavItem text="TablePagination" path="/docs/components/tablepagination" class="pl-6 py-2" />
+<NavItem text="Table" path="/docs/components/Table" class="pl-6 py-2" />
+<NavItem text="TablePagination" path="/docs/components/TablePagination" class="pl-6 py-2" />
 
 <div class="pt-4 pb-2 pl-4 text-xs text-gray-200 font-bold">Date</div>
-<NavItem text="DateDisplay" path="/docs/components/datedisplay" class="pl-6 py-2" />
-<NavItem text="DateField" path="/docs/components/datefield" class="pl-6 py-2" />
-<NavItem text="DatePickerField" path="/docs/components/datepickerfield" class="pl-6 py-2" />
-<NavItem text="DateRange" path="/docs/components/daterange" class="pl-6 py-2" />
-<NavItem text="DateRangeField" path="/docs/components/daterangefield" class="pl-6 py-2" />
-<NavItem text="Duration" path="/docs/components/duration" class="pl-6 py-2" />
-<NavItem text="Month" path="/docs/components/month" class="pl-6 py-2" />
-<NavItem text="MonthList" path="/docs/components/monthlist" class="pl-6 py-2" />
-<NavItem text="MonthListByYear" path="/docs/components/monthlistbyyear" class="pl-6 py-2" />
-<NavItem text="YearList" path="/docs/components/yearlist" class="pl-6 py-2" />
+<NavItem text="dateDisplay" path="/docs/components/dateDisplay" class="pl-6 py-2" />
+<NavItem text="DateField" path="/docs/components/DateField" class="pl-6 py-2" />
+<NavItem text="DatePickerField" path="/docs/components/DatePickerField" class="pl-6 py-2" />
+<NavItem text="DateRange" path="/docs/components/DateRange" class="pl-6 py-2" />
+<NavItem text="DateRangeField" path="/docs/components/DateRangeField" class="pl-6 py-2" />
+<NavItem text="Duration" path="/docs/components/Duration" class="pl-6 py-2" />
+<NavItem text="Month" path="/docs/components/Month" class="pl-6 py-2" />
+<NavItem text="MonthList" path="/docs/components/MonthList" class="pl-6 py-2" />
+<NavItem text="MonthListByYear" path="/docs/components/MonthListByYear" class="pl-6 py-2" />
+<NavItem text="YearList" path="/docs/components/YearList" class="pl-6 py-2" />
 
 <div
   class="py-2 pl-4 mt-4 text-sm text-gray-200 font-bold bg-black/20 border-t border-b border-white/10"
