0983a0556f7bf029f82fce81561bb32827a4ccfb
Sean Lynch <techniq35@gmail.com>
2022-01-04 07:21:33 -0500

    Extract codePreview() and markdownToc() plugins/preprocessors

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,37 +1,6 @@
 import slug from 'rehype-slug';
-import { visit } from 'unist-util-visit';
-import prettier from 'prettier/esm/standalone.mjs';
-import typescriptPlugin from 'prettier/esm/parser-typescript.mjs';
-import sveltePlugin from 'prettier-plugin-svelte';
-import Prism from 'prismjs';
-import 'prism-svelte';
 
-const { format } = prettier;
-
-/**
- * Inject `code` prop to <Preview>
- */
-function codePreview() {
-  function visitor(node) {
-    // TODO: Support updating Preview nested in other markup.  https://stackoverflow.com/questions/14768735/regex-replace-only-inside-tag-need-help-writing-regex
-    // TODO: Support Passing props to preview (isDark, class, etc)
-    if (node.value.startsWith('<Preview>')) {
-      // Get contents between <Preview> tag
-      const code = node.value.match(/<Preview>([^]*)<\/Preview>/)[1];
-
-      const formattedCode = format(code, {
-        parser: 'svelte',
-        plugins: [typescriptPlugin, sveltePlugin],
-      });
-      const highlightedCode = Prism.highlight(formattedCode, Prism.languages.svelte, 'svelte');
-      node.value = `<Preview code="{\`${highlightedCode}\`}" language="svelte">${code}</Preview>`;
-    }
-  }
-
-  return (tree) => {
-    visit(tree, 'html', visitor);
-  };
-}
+import { codePreview } from './src/lib/plugins/remark.js';
 
 export default {
   extensions: ['.md', '.svx'],
