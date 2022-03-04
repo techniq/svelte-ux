b62004e3ade8c44d5395034864fb04c165afc228
Sean Lynch <techniq35@gmail.com>
2022-03-04 08:40:15 -0500

    [TableOrderIcon] Support order as readable store value

--- src/lib/components/TableOrderIcon.svelte
+++ src/lib/components/TableOrderIcon.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+  import type { Readable } from 'svelte/store';
   import { mdiArrowUp } from '@mdi/js';
 
   import type { TableOrderState } from '../stores/tableOrderStore';
   import type { ColumnDef } from '../types/table';
   import Icon from '../components/Icon.svelte';
 
-  export let order: TableOrderState;
+  export let order: TableOrderState | Readable<TableOrderState>;
   export let column: ColumnDef;
 </script>
 
-{#if order.by && (order.by === column.name || order.by === column.orderBy)}
+{#if 'subscribe' in order}
+  {#if $order.by && ($order.by === column.name || $order.by === column.orderBy)}
+    <div
+      class="inline-block transition duration-100 transform"
+      class:rotate-180={$order.direction === 'desc'}
+    >
+      <Icon path={mdiArrowUp} />
+    </div>
+  {/if}
+{:else if order.by && (order.by === column.name || order.by === column.orderBy)}
   <div
     class="inline-block transition duration-100 transform"
     class:rotate-180={order.direction === 'desc'}
