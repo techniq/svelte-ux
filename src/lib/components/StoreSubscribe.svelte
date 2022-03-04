0a9e695dfde84608f33d708867cec1f3a15e42b1
Sean Lynch <techniq35@gmail.com>
2022-03-04 10:34:34 -0500

    Add StoreSubscribe value (aka take 3 for TableOrderIcon store support)

---
+++ src/lib/components/StoreSubscribe.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  /**
+   * Wrapper component to convenient subscribe to a store value when impractical (for example, slot `let:value`)
+   */
+  import type { Readable } from 'svelte/store';
+
+  type T = $$Generic;
+
+  export let value: Readable<T>;
+</script>
+
+<slot value={$value} />
--- src/lib/components/TableOrderIcon.svelte
+++ src/lib/components/TableOrderIcon.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-  import type { Readable } from 'svelte/store';
   import { mdiArrowUp } from '@mdi/js';
 
   import type { TableOrderState } from '../stores/tableOrderStore';
   import type { ColumnDef } from '../types/table';
   import Icon from '../components/Icon.svelte';
 
-  export let order: TableOrderState | Readable<TableOrderState>;
+  export let order: TableOrderState;
   export let column: ColumnDef;
-
-  $: _order = 'subscribe' in order ? $order : order;
 </script>
 
-{#if _order.by && (_order.by === column.name || _order.by === column.orderBy)}
+{#if order.by && (order.by === column.name || order.by === column.orderBy)}
   <div
     class="inline-block transition duration-100 transform"
-    class:rotate-180={_order.direction === 'desc'}
+    class:rotate-180={order.direction === 'desc'}
   >
     <Icon path={mdiArrowUp} />
   </div>
