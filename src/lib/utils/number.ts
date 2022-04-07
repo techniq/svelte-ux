eb8eac8be6c8b3f7b23e961020dab4ee5fcc7643
Sean Lynch <techniq35@gmail.com>
2022-04-06 23:27:14 -0400

    Add randomInteger() number util

--- src/lib/utils/number.ts
+++ src/lib/utils/number.ts
@@ -100,3 +100,10 @@ export function decimalCount(value: number) {
 export function round(value: number, decimals: number) {
   return Number(value.toFixed(decimals));
 }
+
+/**
+ * Get random number between min and max (inclusive)
+ */
+export function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
