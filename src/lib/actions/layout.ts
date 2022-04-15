a4112ec72aef2702ddfb3f05a70e8eda58003255
Sean Lynch <techniq35@gmail.com>
2022-04-15 16:13:21 -0400

    Handle null options

--- src/lib/actions/layout.ts
+++ src/lib/actions/layout.ts
@@ -5,18 +5,17 @@ export function remainingViewportHeight(
   node: HTMLElement,
   options?: { max?: boolean; offset?: number; enabled?: boolean }
 ): SvelteActionReturnType {
-  const max = options?.max ?? false;
-  const offset = options?.offset ?? 0;
-
-  const viewportClientTop = node.getBoundingClientRect().top;
-
   function update(options) {
-    const property = max ? 'max-height' : 'height';
+    const viewportClientTop = node.getBoundingClientRect().top;
+    const property = options?.max ? 'max-height' : 'height';
 
-    if (options.enabled === false) {
+    if (options?.enabled === false) {
       node.style.removeProperty(property);
     } else {
-      node.style.setProperty(property, `calc(100vh - ${viewportClientTop}px - ${offset}px)`);
+      node.style.setProperty(
+        property,
+        `calc(100vh - ${viewportClientTop}px - ${options?.offset ?? 0}px)`
+      );
     }
   }
 
@@ -31,19 +30,19 @@ export function remainingViewportWidth(
   node: HTMLElement,
   options?: { max?: boolean; offset?: number; enabled?: boolean }
 ): SvelteActionReturnType {
-  const max = options?.max ?? false;
-  const offset = options?.offset ?? 0;
-
-  // TODO: Find way to watch/update when viewport location changes (ex. closing side drawer).  Resizer observer does not work for these cases.  Using the absolute positioned sentinel element sounds promising: https://stackoverflow.com/questions/40251082/an-event-or-observer-for-changes-to-getboundingclientrect
-  const viewportClientLeft = node.getBoundingClientRect().left;
-
   function update(options) {
-    const property = max ? 'max-width' : 'width';
+    // TODO: Find way to watch/update when viewport location changes (ex. closing side drawer).  Resizer observer does not work for these cases.  Using the absolute positioned sentinel element sounds promising: https://stackoverflow.com/questions/40251082/an-event-or-observer-for-changes-to-getboundingclientrect
+    const viewportClientLeft = node.getBoundingClientRect().left;
 
-    if (options.enabled === false) {
+    const property = options?.max ? 'max-width' : 'width';
+
+    if (options?.enabled === false) {
       node.style.removeProperty(property);
     } else {
-      node.style.setProperty(property, `calc(100vw - ${viewportClientLeft}px - ${offset}px)`);
+      node.style.setProperty(
+        property,
+        `calc(100vw - ${viewportClientLeft}px - ${options?.offset ?? 0}px)`
+      );
     }
   }
 
