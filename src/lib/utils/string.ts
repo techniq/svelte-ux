465cea8f402b3063301d004e9f4941ba049d3f74
Sean Lynch <techniq35@gmail.com>
2022-04-19 22:29:32 -0400

    Add uniqueId() string util

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -4,3 +4,12 @@
 export function isUpperCase(str: string) {
   return /^[A-Z]*$/.test(str);
 }
+
+/**
+ * Generates a unique Id, with prefix if provided
+ */
+let idCounter = 0;
+export function uniqueId(prefix = '') {
+  var id = ++idCounter;
+  return prefix + id;
+}
