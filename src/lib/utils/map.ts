c61f7f2b19ce7ef1d24ff79c3db1d8f65a2d863c
Sean Lynch <techniq35@gmail.com>
2022-05-19 15:05:39 -0400

    Fix accidently removing export on last change

--- src/lib/utils/map.ts
+++ src/lib/utils/map.ts
@@ -2,7 +2,7 @@
  * Get the value at path of Map.  Useful for nested maps (d3-array group, etc).
  * Similar to lodash get() but for Map instead of Object
  */
-function get<K, V>(map: Map<K, V>, path: string[]) {
+export function get<K, V>(map: Map<K, V>, path: string[]) {
   let key = null;
   let value = map;
   while ((key = path.shift())) {
