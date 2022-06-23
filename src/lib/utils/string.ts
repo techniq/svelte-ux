9ffec628ec541dd99ef298acd1364732f5438405
Sean Lynch <techniq35@gmail.com>
2022-06-23 15:56:49 -0400

    Add truncate() string util

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -13,3 +13,19 @@ export function uniqueId(prefix = '') {
   var id = ++idCounter;
   return prefix + id;
 }
+
+/**
+ * Truncate text with option to keep a number of characters on end.  Inserts ellipsis between parts
+ */
+export function truncate(text: string, totalChars: number, endChars: number = 0) {
+  endChars = Math.min(endChars, totalChars);
+
+  const start = text.slice(0, totalChars - endChars);
+  const end = endChars > 0 ? text.slice(-endChars) : '';
+
+  if (start.length + end.length < text.length) {
+    return start + '…' + end;
+  } else {
+    return text;
+  }
+}
