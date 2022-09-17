94bd4daf16d1a8a4d2f20c54a02bbb23fdd71ed7
Sean Lynch <techniq35@gmail.com>
2022-09-16 23:03:47 -0400

    Add mapStore which wraps `Map` and simplifies syncing state with Svelte

---
+++ src/lib/stores/mapStore.ts
@@ -0,0 +1,35 @@
+import { writable } from 'svelte/store';
+
+/**
+ * Store to wrap `Map` to simplify syncing state (set, delete, clear) with Svelte
+ */
+export function mapStore<TKey, TValue>(
+  initialValues?: ConstructorParameters<typeof Map<TKey, TValue>>[0]
+) {
+  const store = writable<Map<TKey, TValue>>(new Map<TKey, TValue>(initialValues));
+
+  return {
+    subscribe: store.subscribe,
+
+    set(key: TKey, value: TValue) {
+      store.update((map) => {
+        map.set(key, value);
+        return map;
+      });
+    },
+
+    delete(key: TKey) {
+      store.update((map) => {
+        map.delete(key);
+        return map;
+      });
+    },
+
+    clear() {
+      store.update((map) => {
+        map.clear();
+        return map;
+      });
+    },
+  };
+}
