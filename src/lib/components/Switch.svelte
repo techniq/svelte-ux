0871d0b49c19a1a8306cd540bfc00fd9a847eab0
Sean Lynch <techniq35@gmail.com>
2022-10-24 08:29:21 -0400

    [Switch] Support focusing and simplify implementation.

--- src/lib/components/Switch.svelte
+++ src/lib/components/Switch.svelte
@@ -1,48 +1,50 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
   import clsx from 'clsx';
+  import { uniqueId } from '$lib/utils/string';
 
+  export let id: string = uniqueId('switch_');
   export let value: any = undefined;
   export let checked: boolean = false;
   export let disabled: boolean = false;
-  export let id: string = undefined;
   export let size: 'sm' | 'md' | 'lg' = 'lg';
-
-  const dispatch = createEventDispatcher();
-
-  function onClick(e: MouseEvent) {
-    // TODO: Find why to only stop propagation if passed to on:change listener
-    e.stopPropagation();
-
-    if (disabled) return;
-    checked = !checked;
-    dispatch('change', checked);
-  }
 </script>
 
-<div
-  class={clsx(
-    'switch',
-    'border rounded-full flex align-items p-[3px]',
-    {
-      'w-6 h-4': size === 'sm',
-      'w-8 h-5': size === 'md',
-      'w-10 h-6': size === 'lg',
-    },
-    disabled ? 'opacity-50' : 'cursor-pointer',
-    checked ? 'bg-blue-400 border-blue-400' : 'bg-gray-300 border-gray-300',
+<div class="inline-block">
+  <input
+    {id}
+    type="checkbox"
+    bind:checked
+    on:change
+    {value}
+    class="peer appearance-none inline"
+    {disabled}
+  />
 
-    $$props.class
-  )}
-  on:click={onClick}
->
-  <input type="checkbox" bind:checked class="hidden" on:change {value} {id} />
-  <div
+  <label
+    for={id}
     class={clsx(
-      'toggle w-1/2 h-full rounded-full transition-all duration-200 bg-white flex items-center justify-center transform',
-      checked && 'translate-x-full'
+      'switch',
+      'border rounded-full flex align-items p-[3px]',
+      {
+        'w-6 h-4': size === 'sm',
+        'w-8 h-5': size === 'md',
+        'w-10 h-6': size === 'lg',
+      },
+      checked ? 'bg-blue-400 border-blue-400' : 'bg-gray-300 border-gray-300',
+      disabled
+        ? 'opacity-50'
+        : 'cursor-pointer peer-focus-visible:ring-2 ring-accent-400 ring-offset-1',
+
+      $$props.class
     )}
   >
-    <slot {checked} {value} />
-  </div>
+    <div
+      class={clsx(
+        'toggle w-1/2 h-full rounded-full transition-all duration-200 bg-white flex items-center justify-center transform',
+        checked && 'translate-x-full'
+      )}
+    >
+      <slot {checked} {value} />
+    </div>
+  </label>
 </div>
