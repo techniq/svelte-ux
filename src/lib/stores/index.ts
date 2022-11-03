dde477d514d1e1875bec8edb349aee81236dd564
Sean Lynch <techniq35@gmail.com>
2022-11-02 23:05:28 -0400

    Add selectionStore to top-level exports

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -6,6 +6,7 @@ export { default as localStore } from './localStore';
 export { default as mapStore } from './mapStore';
 export { default as matchMedia } from './matchMedia';
 export { default as paginationStore } from './paginationStore';
+export { default as selectionStore } from './selectionStore';
 export { default as tableOrderStore } from './tableOrderStore';
 export { default as timerStore } from './timerStore';
 export { default as uniqueStore } from './uniqueStore';
