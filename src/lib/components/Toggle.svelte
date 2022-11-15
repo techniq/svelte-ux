e74fbd20409aa9f34e861b89235d4c3410c62284
Sean Lynch <techniq35@gmail.com>
2022-11-15 13:02:28 -0500

    [Toggle] Add `toggle`, `toggleOn`, and `toggleOff` events

--- src/lib/components/Toggle.svelte
+++ src/lib/components/Toggle.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
   let on = false;
 
   function toggle() {
     on = !on;
+    dispatch('toggle', on);
+    if (on) {
+      dispatch('toggleOn');
+    } else {
+      dispatch('toggleOff');
+    }
   }
   function toggleOn() {
     on = true;
+    dispatch('toggle', on);
+    dispatch('toggleOn');
   }
   function toggleOff() {
     on = false;
+    dispatch('toggle', on);
+    dispatch('toggleOff');
   }
 </script>
 
