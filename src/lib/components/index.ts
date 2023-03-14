85c7134b14fd6e88a6832b568a29015f085172f0
Sean Lynch <techniq35@gmail.com>
2023-03-14 14:46:36 -0400

    Add ButtonGroup to top-level exports

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -6,6 +6,7 @@ export { default as Backdrop } from './Backdrop.svelte';
 export { default as Badge } from './Badge.svelte';
 export { default as Breadcrumb } from './Breadcrumb.svelte';
 export { default as Button } from './Button.svelte';
+export { default as ButtonGroup } from './ButtonGroup.svelte';
 export { default as Card } from './Card.svelte';
 export { default as Checkbox } from './Checkbox.svelte';
 export { default as CircularProgress } from './CircularProgress.svelte';
