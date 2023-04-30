609b049206a8d377d65ae7f3fd30124f26338e2e
Sean Lynch <techniq35@gmail.com>
2023-04-30 11:04:39 -0400

    [TableOfContents] Highlight active heading based on scroll

--- src/lib/components/TableOfContents.svelte
+++ src/lib/components/TableOfContents.svelte
@@ -1,28 +1,57 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onDestroy, onMount } from 'svelte';
 
   import { buildTree } from '../utils/array';
   import { cls } from '../utils/styles';
   import TreeList from './TreeList.svelte';
 
+  let activeHeadingId = '';
   let headings = [];
+  let nodes = [];
+
+  function onScroll(e) {
+    activeHeadingId = headings?.find(
+      (heading) => heading.element.offsetTop >= e.target.scrollTop + e.target.offsetTop
+    )?.id;
+  }
 
   onMount(() => {
-    const headingItems = Array.from(
-      document.querySelector('main').querySelectorAll('h1,h2,h3,h4,h5,h6')
-    ).map((el) => ({ level: Number(el.tagName[1]), id: el.id, name: el.innerHTML }));
-    headings = buildTree(headingItems);
+    const mainEl = document.querySelector('main');
+
+    headings = Array.from(mainEl.querySelectorAll('h1,h2,h3,h4,h5,h6'), (el) => ({
+      id: el.id,
+      name: el.innerHTML,
+      level: Number(el.tagName[1]),
+      element: el,
+    }));
+    nodes = buildTree(headings);
+
+    mainEl.addEventListener('scroll', onScroll, { passive: true });
+    // set first heading until scrolled
+    activeHeadingId = headings[0]?.id;
+  });
+
+  onDestroy(() => {
+    const mainEl = document.querySelector('main');
+    mainEl.removeEventListener('scroll', onScroll);
   });
 </script>
 
 <TreeList
-  nodes={headings}
-  classes={{ li: (node) => cls('ml-4', node.level === 1 && 'mb-2') }}
+  {nodes}
+  classes={{ li: (node) => cls(node.level === 1 ? 'mb-2' : 'ml-2') }}
   {...$$restProps}
   let:node
 >
   <slot {node}>
-    <a href="#{node.id}" class={cls(node.level === 1 ? 'font-semibold' : 'text-sm')}>
+    <a
+      href="#{node.id}"
+      class={cls(
+        'block px-2',
+        node.level === 1 ? 'font-semibold' : 'text-sm',
+        node.id === activeHeadingId && 'bg-black/[0.08] rounded-lg'
+      )}
+    >
       {node.name}
     </a>
   </slot>
