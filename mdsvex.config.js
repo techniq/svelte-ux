c081d25b6c9edd527603224d79b9890217593e88
Sean Lynch <techniq35@gmail.com>
2023-05-01 20:06:30 -0400

    Remove remark codePreview plugin (replaced by svelte preprocessor)

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,11 +1,8 @@
 import slug from 'rehype-slug';
 
-import { codePreview } from './src/lib/plugins/remark.js';
-
 export default {
   extensions: ['.md', '.svx'],
   layout: './src/docs/Layout.svelte',
-  // remarkPlugins: [codePreview],
   rehypePlugins: [slug],
   // highlight: false
 };
