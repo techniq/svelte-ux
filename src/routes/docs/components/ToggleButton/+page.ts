eaa164305dbeaf1d724fc9b8c8b59f591e984a90
Sean Lynch <techniq35@gmail.com>
2023-05-05 14:19:49 -0400

    Add ToggleButton description

---
+++ src/routes/docs/components/ToggleButton/+page.ts
@@ -0,0 +1,8 @@
+export function load() {
+  return {
+    meta: {
+      description:
+        'Simplifies using Button/Toggle, and helps coordinate unmounting of toggled content (i.e. delays unmount to allow children to transition (ex. Drawer/Dialog))',
+    },
+  };
+}
