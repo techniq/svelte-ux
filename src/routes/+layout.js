d20eef47443712c8140da23e1e56fd1016bfb005
Sean Lynch <techniq35@gmail.com>
2023-05-15 23:19:57 -0400

    Change layout.js to layout.ts

--- src/routes/+layout.js
+++
@@ -1,2 +0,0 @@
-// Disable server-side rendering until AppLayout shift is fixed (issue #22).  Used to be `ssr: false` in `svelte.config.js`
-export const ssr = false;
---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+// Disable server-side rendering until AppLayout shift is fixed (issue #22)
+export const ssr = false;
