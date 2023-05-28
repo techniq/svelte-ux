42706d1b58b5020b473c7ce95e3e9098b7891818
Sean Lynch <techniq35@gmail.com>
2023-05-28 10:33:55 -0400

    Always show Code/Preview copy buttons on top (with background blur)

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -20,10 +20,10 @@
           <code class="language-{language}">{@html highlightedCode}</code>
       </pre>
 
-      <div class="absolute top-0 right-0 p-2">
+      <div class="absolute top-0 right-0 p-2 z-10">
         <Button
           icon={mdiContentCopy}
-          class="text-white/70 hover:bg-white/20 py-1"
+          class="text-white/70 hover:bg-white/20 py-1 backdrop-blur-md"
           size="sm"
           on:click={() => navigator.clipboard.writeText(code ?? '')}
         >
