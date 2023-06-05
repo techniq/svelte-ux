8f0b89c0779860cf1885083f61e55619a7cb0b9b
Sean Lynch <techniq35@gmail.com>
2023-06-04 23:42:40 -0400

    [AppLayout] Allow passing `areas` for more control (ex. full-width header).  Use `style:--var` instead of `cssVars` actions.  Add `classes` prop.  Leverage more tailwind JIT

--- src/lib/components/AppLayout.svelte
+++ src/lib/components/AppLayout.svelte
@@ -7,21 +7,32 @@
   import Backdrop from './Backdrop.svelte';
 
   import { mdScreen } from '../stores/matchMedia';
-  import cssVars from '../actions/cssVars';
   import { cls } from '../utils/styles';
 
   export let navWidth = 240;
+  /** Use areas="'header header' 'aside main'" for full-width header */
+  export let areas = "'aside header' 'aside main'";
+
+  export let classes: {
+    root?: string;
+    aside?: string;
+    nav?: string;
+  } = {};
 
   let isDesktop = mdScreen;
   $: temporaryDrawer = !$isDesktop;
-
-  $: styleVars = {
-    drawerWidth: $showDrawer ? `${navWidth}px` : '0',
-    navWidth: navWidth + 'px',
-  };
 </script>
 
-<div class="layout grid h-screen" use:cssVars={styleVars}>
+<div
+  class={cls(
+    'layout grid grid-cols-[auto,1fr] grid-rows-[64px,1fr] h-screen',
+    classes.root,
+    $$props.class
+  )}
+  style:--drawerWidth="{$showDrawer ? navWidth : 0}px"
+  style:--navWidth="{navWidth}px"
+  style:--areas={areas}
+>
   <!-- Render backdrop first to fix stacking order with <aside> nav -->
   {#if $showDrawer && temporaryDrawer}
     <Backdrop on:click={() => ($showDrawer = false)} class="z-20" />
@@ -30,10 +41,11 @@
   <aside
     class={cls(
       'w-[var(--drawerWidth)] transition-all duration-500 overflow-hidden',
-      temporaryDrawer && 'fixed h-full z-30 temporary'
+      temporaryDrawer && 'fixed h-full z-30 elevation-10',
+      classes.aside
     )}
   >
-    <nav class="nav h-full overflow-scroll bg-[#282b31] w-[var(--navWidth)]">
+    <nav class={cls('nav h-full overflow-scroll bg-[#282b31] w-[var(--navWidth)]', classes.nav)}>
       <slot name="nav" />
     </nav>
   </aside>
@@ -41,23 +53,14 @@
   <slot />
 </div>
 
-<style lang="postcss">
+<style>
   /* .container is used by tailwind */
   .layout {
-    grid-template-columns: auto 1fr;
-    grid-template-rows: 64px 1fr;
-    grid-template-areas:
-      'aside header'
-      'aside main';
+    grid-template-areas: var(--areas);
   }
 
   aside {
     grid-area: aside;
-  }
-
-  aside.temporary {
-    box-shadow: 0px 8px 10px -5px rgba(0, 0, 0, 0.2), 0px 16px 24px 2px rgba(0, 0, 0, 0.14),
-      0px 6px 30px 5px rgba(0, 0, 0, 0.12);
   }
 
   .layout :global(> header) {
