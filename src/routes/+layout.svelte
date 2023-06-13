4f52497d01e9b867f73b581d5a54420a242a91ee
Sean Lynch <techniq35@gmail.com>
2023-06-12 23:19:19 -0400

    Change side nav background to palette color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
   createTheme({
     AppBar: 'bg-accent-500 text-white shadow-md',
     AppLayout: {
-      nav: 'bg-[#282b31] py-4',
+      nav: 'bg-neutral-800 py-4',
     },
     NavItem: {
       root: 'text-gray-400 hover:text-white hover:bg-gray-300/10 [&:where(.is-active)]:text-sky-400 [&:where(.is-active)]:bg-gray-500/10 pl-6 py-2',
