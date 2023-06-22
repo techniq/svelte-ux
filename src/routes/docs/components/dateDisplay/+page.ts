c4773e49aa258b128850c8efb0d8a654503ae167
Sean Lynch <techniq35@gmail.com>
2023-06-22 12:01:23 -0400

    [dateDisplay] Add features list

--- src/routes/docs/components/dateDisplay/+page.ts
+++ src/routes/docs/components/dateDisplay/+page.ts
@@ -1,5 +1,11 @@
 export async function load() {
   return {
-    meta: {},
+    meta: {
+      features: [
+        'Pass `periodType` along with `variant` for quick formatting',
+        'Pass `format` for <a href="https://date-fns.org/v2.30.0/docs/format" class="underline underline-offset-2 decoration-gray-400">greater control</a>',
+        'By default, will be formatted using `date.totoLocaleString()`',
+      ],
+    },
   };
 }
