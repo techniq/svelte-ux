eb8d78ddc549024361644c0f3e8ff7906b138a0b
Sean Lynch <techniq35@gmail.com>
2023-06-22 21:04:51 -0400

    Fix typo

--- src/routes/docs/components/dateDisplay/+page.ts
+++ src/routes/docs/components/dateDisplay/+page.ts
@@ -4,7 +4,7 @@ export async function load() {
       features: [
         'Pass `periodType` along with `variant` for quick formatting',
         'Pass `format` for <a href="https://date-fns.org/v2.30.0/docs/format" class="underline underline-offset-2 decoration-gray-400">greater control</a>',
-        'By default, will be formatted using `date.totoLocaleString()`',
+        'By default, will be formatted using `date.toLocaleString()`',
       ],
     },
   };
