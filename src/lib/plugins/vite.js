50c8d1deafacb8155ccd224991c13ade44f2a398
Sean Lynch <techniq35@gmail.com>
2023-09-13 21:43:20 -0400

    Inline vite-plugin-sveld to fix svelte 4.x compat

---
+++ src/lib/plugins/vite.js
@@ -0,0 +1,32 @@
+import { ComponentParser } from 'sveld';
+import sveltePreprocess from 'svelte-preprocess';
+import { preprocess } from 'svelte/compiler';
+import qs from 'node:querystring';
+
+export function sveld() {
+  return {
+    name: 'vite-plugin-sveld',
+    async transform(src, id) {
+      const query = qs.parse(id.split('?')[1]);
+
+      if ('raw' in query && 'sveld' in query) {
+        const raw = JSON.parse(src.split('export default ')[1]);
+
+        let { code } = await preprocess(raw, sveltePreprocess(), {
+          filename: id,
+        });
+        const data = new ComponentParser({
+          verbose: false,
+        }).parseSvelteComponent(code, {
+          filePath: id,
+          moduleName: id,
+        });
+
+        return {
+          code: `export default ${JSON.stringify(data)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import sveld from 'vite-plugin-sveld';
+import { sveld } from './src/lib/plugins/vite';
 
 export default defineConfig({
   plugins: [sveltekit(), sveld()],
