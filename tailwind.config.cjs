e9054ac9420e1c42a6a03ed720ee5eb29459cf66
Sean Lynch <techniq35@gmail.com>
2023-10-11 23:31:17 -0400

    Add CSS grid RAM (reapeat, auto-fill, minmax) components to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,8 +17,24 @@ module.exports = {
     require('./src/lib/plugins/tailwind.cjs'),
     require('@tailwindcss/typography'),
 
-    // Expose color palette as CSS variables (--color-xxx-yyy) - https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
-    plugin(function ({ addBase, theme }) {
+    plugin(function ({ addBase, addComponents, theme }) {
+      // Consider moving to tailwind plugin
+      addComponents({
+        '.grid-cols-xs': {
+          '@apply grid-cols-[repeat(auto-fill,minmax(200px,1fr))]': {},
+        },
+        '.grid-cols-sm': {
+          '@apply grid-cols-1 sm:grid-cols-[repeat(auto-fill,minmax(300px,1fr))]': {},
+        },
+        '.grid-cols-md': {
+          '@apply grid-cols-1 sm:grid-cols-[repeat(auto-fill,minmax(400px,1fr))]': {},
+        },
+        '.grid-cols-lg': {
+          '@apply grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(600px,1fr))]': {},
+        },
+      });
+
+      // Expose color palette as CSS variables (--color-xxx-yyy) - https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
       function extractColorVars(colorObj, colorGroup = '') {
         return Object.keys(colorObj).reduce((vars, colorKey) => {
           const value = colorObj[colorKey];
