79ac787d6051e608ef60de26225e536c3c3dfe7a
willnationsdev <willnationsdev@gmail.com>
2023-11-08 00:27:12 -0600

    Add <Maybe> component.

---
+++ src/lib/components/Maybe.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let self: ConstructorOfATypedSvelteComponent | undefined | null = undefined;
+	export { self as this }
+
+	let component;
+
+</script>
+
+{#if self}
+	<svelte:component bind:this={component} this={self} {...$$restProps}>
+		<slot {component} />
+	</svelte:component>
+{:else}
+	<slot />
+{/if}
