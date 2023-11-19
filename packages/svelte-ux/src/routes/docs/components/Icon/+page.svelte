60702a5ddff4e60abbc5f0334cdddc25d2e8b294
Sean Lynch <techniq35@gmail.com>
2023-11-19 10:30:26 -0500

    [Icon] Remove duplicate example

--- packages/svelte-ux/src/routes/docs/components/Icon/+page.svelte
+++ packages/svelte-ux/src/routes/docs/components/Icon/+page.svelte
@@ -21,7 +21,6 @@
 <h2>Material Design icons</h2>
 
 <Preview>
-  <Icon path={mdiAccount} />
   <Icon data={mdiAccount} />
   <Icon
     svg={'<svg width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z"/></svg>'}
