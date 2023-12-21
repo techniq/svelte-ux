07803ca73e406b22893a936d95459facf7a2a574
jycouet <jycouet@gmail.com>
2023-12-21 21:08:17 +0100

    rmv tmp

--- packages/svelte-ux/src/routes/+layout.svelte
+++ packages/svelte-ux/src/routes/+layout.svelte
@@ -19,7 +19,6 @@
   import type { PageData } from './$types';
 
   export let data: PageData;
-  console.log(`data`, data);
 
   const baseGh = 'https://github.com/techniq/svelte-ux';
   $: ghLink = data.pr_id ? `${baseGh}/pull/${data.pr_id}` : baseGh;
