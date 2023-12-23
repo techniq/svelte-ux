460ceec3145bcd0e089cc99f488f03638e0cd413
Sean Lynch <techniq35@gmail.com>
2023-12-23 12:53:54 -0500

    Add styles (with daisy) as top-level export

--- packages/svelte-ux/src/lib/index.ts
+++ packages/svelte-ux/src/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './actions';
 export * from './components';
 export * from './stores';
+export * from './styles';
 export * from './types';
 // TODO: Conflic Duration Component & Type
 export * from './utils';
---
+++ packages/svelte-ux/src/lib/styles/index.ts
@@ -0,0 +1,1 @@
+import * as daisy from './daisy';
