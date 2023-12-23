387dac333567dbdd5d9b2ed9b07e28061430afaa
Sean Lynch <techniq35@gmail.com>
2023-12-23 11:32:16 -0500

    Read themes/colors from tailwind config, including Daisy UI (with mapping).  Supports generating optional colors and further extension and flexability.

--- packages/svelte-ux/src/lib/styles/daisy.ts
+++ packages/svelte-ux/src/lib/styles/daisy.ts
@@ -1,3 +1,37 @@
+import daisyThemes from 'daisyui/src/theming/themes';
+import { themeOrder } from 'daisyui/src/theming/themeDefaults';
+
+import { sortFunc } from '../utils/sort';
+
+const daisyColorMap = {
+  'base-100': 'surface-100',
+  'base-200': 'surface-200',
+  'base-300': 'surface-300',
+  'base-content': 'surface-content',
+};
+
+/**
+ *  Map Daisy UI color names to Svelte UX names, and sort themes
+ */
+function mapColorsName(themes, colorMap) {
+  return Object.fromEntries(
+    Object.entries(themes)
+      .map(([themeName, colors]) => {
+        return [
+          themeName,
+          Object.fromEntries(
+            Object.entries(colors).map(([key, value]) => {
+              return [colorMap[key] ?? key, value];
+            })
+          ),
+        ];
+      })
+      .sort(sortFunc(([themeName]) => themeOrder.indexOf(themeName)))
+  );
+}
+
+export const themes = mapColorsName(daisyThemes, daisyColorMap);
+
 export const lightThemes = [
   'light',
   'acid',
