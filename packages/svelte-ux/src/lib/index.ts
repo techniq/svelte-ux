543a8074a23042b0a9e714536c5a6add5a2d4018
Sean Lynch <techniq35@gmail.com>
2023-12-26 22:20:34 -0500

    Remove daisy from top-level exports

--- packages/svelte-ux/src/lib/index.ts
+++ packages/svelte-ux/src/lib/index.ts
@@ -1,6 +1,5 @@
 export * from './actions';
 export * from './components';
 export * from './stores';
-export * from './styles';
 export * from './types';
 export * from './utils';
--- packages/svelte-ux/src/lib/styles/index.ts
+++
@@ -1,1 +0,0 @@
-import * as daisy from './daisy';
