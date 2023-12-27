14d3d753e6a659dab7ac4c15d8e72739a1dae97d
Sean Lynch <techniq35@gmail.com>
2023-12-26 22:20:34 -0500

    Remove daisy from top-level exports

--- packages/svelte-ux/src/lib/index.ts
+++ packages/svelte-ux/src/lib/index.ts
@@ -1,7 +1,6 @@
 export * from './actions';
 export * from './components';
 export * from './stores';
-export * from './styles';
 export * from './types';
 // TODO: Conflic Duration Component & Type
 export * from './utils';
