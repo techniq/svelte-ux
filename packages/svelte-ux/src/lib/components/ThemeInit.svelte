b9f1ad96cef9ce1b682bac952571f0e99a5c3b57
Daniel Imfeld <daniel@imfeld.dev>
2024-01-03 17:29:27 -1000

    add ThemeInit component that place script inside <head> tag (#176)
    * add ThemeInit component that place script inside <head> tag
    
    * Fix bug
    
    * proper sorting
    
    * instantiate ThemeInit inside Settings component
    
    * add comment about stringified function

---
+++ packages/svelte-ux/src/lib/components/ThemeInit.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { createHeadSnippet } from '../styles/theme';
+  import { getSettings } from './settings';
+
+  const darkThemes = getSettings().themes?.dark ?? [];
+
+  let headSnippet = createHeadSnippet(darkThemes);
+</script>
+
+<svelte:head>
+  {@html headSnippet}
+</svelte:head>
