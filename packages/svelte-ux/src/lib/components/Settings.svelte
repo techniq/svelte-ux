432a886737ca6b299b705ce0938999a948dd6c30
Daniel Imfeld <daniel@imfeld.dev>
2024-01-03 17:29:27 -1000

    add ThemeInit component that place script inside <head> tag (#176)
    * add ThemeInit component that place script inside <head> tag
    
    * Fix bug
    
    * proper sorting
    
    * instantiate ThemeInit inside Settings component
    
    * add comment about stringified function

--- packages/svelte-ux/src/lib/components/Settings.svelte
+++ packages/svelte-ux/src/lib/components/Settings.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+  import ThemeInit from './ThemeInit.svelte';
   import { settings as setSettings, type Settings } from './settings';
 
   export let settings: Settings;
+  /** Include the ThemeInit component to improve SSR compatibility. */
+  export let themeInit = true;
 
   setSettings(settings);
 </script>
 
+{#if themeInit}
+  <ThemeInit />
+{/if}
 <slot />
