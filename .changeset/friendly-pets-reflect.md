4f929ff9d46f21b436c1043fd678e59e5ae11706
Daniel Imfeld <daniel@imfeld.dev>
2024-01-03 17:29:27 -1000

    add ThemeInit component that place script inside <head> tag (#176)
    * add ThemeInit component that place script inside <head> tag
    
    * Fix bug
    
    * proper sorting
    
    * instantiate ThemeInit inside Settings component
    
    * add comment about stringified function

---
+++ .changeset/friendly-pets-reflect.md
@@ -0,0 +1,5 @@
+---
+"svelte-ux": patch
+---
+
+Add ThemeInit component to prevent flash of unstyled content when SSR is enabled
--- packages/svelte-ux/src/lib/components/index.ts
+++ packages/svelte-ux/src/lib/components/index.ts
@@ -83,6 +83,7 @@ export { default as Tab } from './Tab.svelte';
 export { default as Tabs } from './Tabs.svelte';
 export { default as TextField } from './TextField.svelte';
 export { default as ThemeButton } from './ThemeButton.svelte';
+export { default as ThemeInit } from './ThemeInit.svelte';
 export { default as Tilt } from './Tilt.svelte';
 export { default as Toggle } from './Toggle.svelte';
 export { default as ToggleButton } from './ToggleButton.svelte';
