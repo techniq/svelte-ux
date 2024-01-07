cb502d8e10ead76a295fc6eb3fa5f0b657395371
Sean Lynch <techniq35@gmail.com>
2024-01-07 01:01:32 -0500

    Add explicit daisy themeName list to hopefully fix build (similar to skeleton)

--- packages/svelte-ux/src/lib/styles/daisy.ts
+++ packages/svelte-ux/src/lib/styles/daisy.ts
@@ -1,6 +1,41 @@
 import daisyThemes from 'daisyui/src/theming/themes';
-import { themeOrder } from 'daisyui/src/theming/themeDefaults';
+// import { themeOrder } from 'daisyui/src/theming/themeDefaults'; // breaks build
 import { sortFunc } from '../utils/sort';
+
+const themeNames = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+  'cmyk',
+  'autumn',
+  'business',
+  'acid',
+  'lemonade',
+  'night',
+  'coffee',
+  'winter',
+  'dim',
+  'nord',
+  'sunset',
+];
 
 const daisyColorMap = {
   'base-100': 'surface-100',
@@ -27,7 +62,7 @@ function mapColorsName(themes: typeof daisyThemes, colorMap: typeof daisyColorMa
           ),
         ];
       })
-      .sort(sortFunc(([themeName]) => themeOrder.indexOf(themeName)))
+      .sort(sortFunc(([themeName]) => themeNames.indexOf(themeName)))
   );
 }
 
