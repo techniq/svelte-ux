18d942def4412ee1657850111bf30b9241ed7750
Sean Lynch <techniq35@gmail.com>
2024-01-10 22:29:50 -0500

    [MenuButton] Support hiding `menuIcon` and dispatch `change` event

--- packages/svelte-ux/src/lib/components/MenuButton.svelte
+++ packages/svelte-ux/src/lib/components/MenuButton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
   import type { ComponentProps } from '$lib/types';
   import { mdiMenuDown } from '@mdi/js';
 
@@ -10,10 +11,12 @@
   import MenuItem from './MenuItem.svelte';
   import { getComponentClasses } from './theme';
 
+  const dispatch = createEventDispatcher<{ change: { value: any } }>();
+
   export let options: Array<{ label: string; value: any; icon?: string }>;
   export let value: any = null;
   export let menuProps: ComponentProps<Menu> = { placement: 'bottom-start' };
-  export let menuIcon = mdiMenuDown;
+  export let menuIcon: string | null = mdiMenuDown;
   $: selected = options?.find((x) => x.value === value);
 
   export let classes: {
@@ -37,15 +40,17 @@
     </span>
   </slot>
 
-  <Icon
-    path={menuIcon}
-    class={cls(
-      'opacity-50 transform transition-all -mr-2 duration-300',
-      open && '-rotate-180',
-      settingsClasses.icon,
-      classes.icon
-    )}
-  />
+  {#if menuIcon}
+    <Icon
+      path={menuIcon}
+      class={cls(
+        'opacity-50 transform transition-all -mr-2 duration-300',
+        open && '-rotate-180',
+        settingsClasses.icon,
+        classes.icon
+      )}
+    />
+  {/if}
 
   <Menu
     {open}
@@ -60,7 +65,10 @@
           <MenuItem
             icon={option.icon}
             selected={option.value === value}
-            on:click={() => (value = option.value)}
+            on:click={() => {
+              value = option.value;
+              dispatch('change', { value });
+            }}
           >
             {option.label}
           </MenuItem>
