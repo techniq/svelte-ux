19df8cdae0fb60fa831a18e80ab32cddab2e6214
Sean Lynch <techniq35@gmail.com>
2024-01-12 21:39:51 -0500

    Restore using `$env/dynamic/private` (doesn't appear to solve Safari issue)

--- packages/svelte-ux/src/routes/+layout.server.ts
+++ packages/svelte-ux/src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
-// import { env } from '$env/dynamic/private';
+import { env } from '$env/dynamic/private';
 
 import themes from '../../themes.json';
 import { getThemeNames } from '$lib/styles/theme';
@@ -16,6 +16,6 @@ export async function load({ url }) {
   return {
     themes: getThemeNames(themes),
     // themes: { light: ['light'], dark: ['dark'] },
-    // pr_id: env.VERCEL_GIT_PULL_REQUEST_ID, // TODO: Re-add once SvelteKit updated to `2.3.2+` - https://github.com/sveltejs/kit/releases/tag/%40sveltejs%2Fkit%402.3.2
+    pr_id: env.VERCEL_GIT_PULL_REQUEST_ID,
   };
 }
