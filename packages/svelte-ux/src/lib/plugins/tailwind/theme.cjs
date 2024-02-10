04151561597dcecf20cd7b8e0bf3ee09ee8b39f8
Sean Lynch <techniq35@gmail.com>
2024-02-10 10:19:44 -0500

    Add default light/dark themes if none are provided

--- packages/svelte-ux/src/lib/plugins/tailwind/theme.cjs
+++ packages/svelte-ux/src/lib/plugins/tailwind/theme.cjs
@@ -1,11 +1,35 @@
+const colors = require('tailwindcss/colors');
 const { createThemeColors, processThemeColors } = require('../../styles/theme');
+
+const defaultThemes = {
+  light: {
+    primary: colors['blue']['500'],
+    'primary-content': 'white',
+    secondary: colors['blue']['500'],
+    'surface-100': 'white',
+    'surface-200': colors['gray']['100'],
+    'surface-300': colors['gray']['300'],
+    'surface-content': colors['gray']['900'],
+    'color-scheme': 'light',
+  },
+  dark: {
+    primary: colors['blue']['500'],
+    'primary-content': 'white',
+    secondary: colors['blue']['500'],
+    'surface-100': colors['zinc']['800'],
+    'surface-200': colors['zinc']['900'],
+    'surface-300': colors['zinc']['950'],
+    'surface-content': colors['zinc']['100'],
+    'color-scheme': 'dark',
+  },
+};
 
 /**
  * Inject all Tailwind config themes (`{ ux: { themes: [] }}`) into base CSS
  * @param {'rgb' | 'hsl' | 'oklch'} colorSpace
  */
 function injectThemes(colorSpace, addBase, config) {
-  const themes = config('ux.themes');
+  const themes = config('ux.themes') ?? defaultThemes;
   const themeRoot = config('ux.themeRoot') ?? ':root';
 
   const cssThemes = {};
