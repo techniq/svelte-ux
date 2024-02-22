a049fe2868148709ad800bbf6cdc7f556795f963
Sean Lynch <techniq35@gmail.com>
2024-02-22 07:12:18 -0500

    Add `classes` to Card to Header (#268)

--- packages/svelte-ux/src/lib/components/Header.svelte
+++ packages/svelte-ux/src/lib/components/Header.svelte
@@ -8,19 +8,28 @@
   let className: string | undefined = undefined;
   export { className as class };
 
+  export let classes: {
+    root?: string;
+    container?: string;
+    title?: string;
+    subheading?: string;
+  } = {};
+
   const settingsClasses = getComponentClasses('Header');
 </script>
 
-<div class={cls('Header', 'flex items-center gap-4', settingsClasses.root, className)}>
+<div
+  class={cls('Header', 'flex items-center gap-4', settingsClasses.root, classes.root, className)}
+>
   <slot name="avatar" />
 
-  <div class="flex-1">
+  <div class={cls('flex-1', classes.container)}>
     <slot name="title">
       {#if title}
         {#if Array.isArray(title)}
           <Breadcrumb items={title} class="text-lg" />
         {:else}
-          <div class="text-lg">{title}</div>
+          <div class={cls('text-lg', classes.title)}>{title}</div>
         {/if}
       {/if}
     </slot>
@@ -28,9 +37,12 @@
     <slot name="subheading">
       {#if subheading}
         {#if Array.isArray(subheading)}
-          <Breadcrumb items={subheading} class="text-sm text-surface-content/50" />
+          <Breadcrumb
+            items={subheading}
+            class={cls('text-sm text-surface-content/50', classes.subheading)}
+          />
         {:else}
-          <div class="text-sm text-surface-content/50">{subheading}</div>
+          <div class={cls('text-sm text-surface-content/50', classes.subheading)}>{subheading}</div>
         {/if}
       {/if}
     </slot>
