77ef905abe24b1427105e6037a7bb0e4acc84cdf
Sean Lynch <techniq35@gmail.com>
2024-04-17 08:57:52 -0400

    Add BarStack/Progress related references

--- packages/svelte-ux/src/routes/docs/components/Progress/+page.ts
+++ packages/svelte-ux/src/routes/docs/components/Progress/+page.ts
@@ -8,7 +8,7 @@ export async function load() {
       api,
       source,
       pageSource,
-      related: ['components/ProgressCircle'],
+      related: ['components/BarStack', 'components/ProgressCircle'],
     },
   };
 }
