2cc81d6b7ec135dde8f9f2cd773a72cc46231e11
Sean Lynch <techniq35@gmail.com>
2024-05-18 10:26:11 -0400

    Prettier format

--- packages/create-svelte-ux/bin.js
+++ packages/create-svelte-ux/bin.js
@@ -190,7 +190,7 @@ function copy(
 
 function sourcePath(/** @type {string} */ file) {
   let __dirname = path.dirname(fileURLToPath(import.meta.url));
-  return path.resolve(__dirname, file)
+  return path.resolve(__dirname, file);
 }
 
 function pCancel(cancelText = 'Operation cancelled.') {
