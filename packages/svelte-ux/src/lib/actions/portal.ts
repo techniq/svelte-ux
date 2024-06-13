dd9f77c73078e60690028f13c917d49e2ea42e7e
Tim Haasdyk <tim_haasdyk@sil.org>
2024-06-13 14:07:53 +0200

    Fix .contains() does not mean it's a child and using old options object

--- packages/svelte-ux/src/lib/actions/portal.ts
+++ packages/svelte-ux/src/lib/actions/portal.ts
@@ -21,37 +21,41 @@ export const portal: Action<HTMLElement, PortalOptions> = (node, options) => {
     fallbackTarget: node.closest('.PortalTarget') ?? document.querySelector('.PortalTarget'),
     originalParent: node.parentElement,
   };
-  moveNode(node, options, targets);
+  let currentTarget = moveNode(node, options, targets);
 
   return {
     update(options) {
-      moveNode(node, options, targets);
+      currentTarget = moveNode(node, options, targets);
     },
     destroy() {
-      const target = getTarget(options, targets.fallbackTarget);
       // If target still contains node that was moved, remove it.  Not sure if required
-      if (target?.contains(node)) {
-        target.removeChild(node);
+      if (currentTarget && node.parentElement === currentTarget) {
+        currentTarget.removeChild(node);
       }
     },
   };
 };
 
-function moveNode(node: HTMLElement, options: PortalOptions = {}, targets: PortalTargets) {
+function moveNode(
+  node: HTMLElement,
+  options: PortalOptions = {},
+  targets: PortalTargets
+): Element | null {
   const enabled = typeof options === 'boolean' ? options : options.enabled;
   if (enabled === false) {
     // Put it back where it came from
     if (targets.originalParent !== node.parentElement) {
       targets.originalParent?.appendChild(node);
     }
-    return;
+    return targets.originalParent;
   }
 
   const target = getTarget(options, targets.fallbackTarget);
   target?.appendChild(node);
+  return target;
 }
 
-function getTarget(options: PortalOptions = {}, fallbackTarget: Element | null) {
+function getTarget(options: PortalOptions = {}, fallbackTarget: Element | null): Element | null {
   const target = typeof options === 'object' ? options.target : undefined;
   if (target instanceof HTMLElement) {
     return target;
