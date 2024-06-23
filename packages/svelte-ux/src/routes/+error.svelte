f76424bc18404bc471916910520ed74fd28a9a62
Sean Lynch <techniq35@gmail.com>
2024-06-23 15:01:00 -0400

    Remove no longer available error details

--- packages/svelte-ux/src/routes/+error.svelte
+++ packages/svelte-ux/src/routes/+error.svelte
@@ -11,28 +11,10 @@
     {#if $page.error}
       <div>
         <h2 class="text-lg font-bold mb-1">Message:</h2>
-        <pre class="ml-2 p-4 border rounded-md text-xs bg-surface-content/80 text-surface-100">
-        {$page.error.message}
-      </pre>
+        <pre
+          class="ml-2 p-4 border border-danger-300 rounded-md text-xs bg-danger-50 text-danger-content">{$page
+            .error.message}</pre>
       </div>
-
-      {#if $page.error.frame}
-        <div>
-          <h2 class="text-lg font-bold mb-1">Frame:</h2>
-          <pre class="ml-2 p-4 border rounded-md text-xs bg-surface-content/80 text-surface-100">
-          {$page.error.frame}
-        </pre>
-        </div>
-      {/if}
-
-      {#if $page.error.stack}
-        <div>
-          <h2 class="text-lg font-bold mb-1">Stack:</h2>
-          <pre class="ml-2 p-4 border rounded-md text-xs bg-surface-content/80 text-surface-100">
-          {$page.error.stack}
-        </pre>
-        </div>
-      {/if}
     {/if}
   </div>
 </div>
