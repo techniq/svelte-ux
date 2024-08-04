98581350d5173b702831c7bceffb21077ef3f27a
Sean Lynch <techniq35@gmail.com>
2024-08-04 19:37:16 -0400

    Replace `adapter-auto` with `adapter-cloudflare`

--- packages/svelte-ux/svelte.config.js
+++ packages/svelte-ux/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
