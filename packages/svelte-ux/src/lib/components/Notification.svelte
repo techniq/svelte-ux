1d5fe54e0194db9a7877212556d4d66e74cb6a09
Sean Lynch <techniq35@gmail.com>
2024-10-03 23:17:23 -0400

    feat(Notification): Add `variant` (default, fill) and `color` (theme) props

--- packages/svelte-ux/src/lib/components/Notification.svelte
+++ packages/svelte-ux/src/lib/components/Notification.svelte
@@ -2,7 +2,7 @@
   import { createEventDispatcher, type ComponentProps } from 'svelte';
   import { fly } from 'svelte/transition';
   import { quadIn } from 'svelte/easing';
-  import { cls } from '@layerstack/tailwind';
+  import { cls, type ThemeColors } from '@layerstack/tailwind';
 
   import { mdiClose } from '@mdi/js';
 
@@ -16,9 +16,12 @@
   export let icon: string | undefined = undefined;
   export let actions: Record<string, Function> = {};
 
+  export let color: ThemeColors = 'primary';
+  export let variant: 'default' | 'fill' = 'default';
+  export let actionsPlacement: 'inline' | 'below' | 'split' = 'inline';
+
   export let open: boolean = true;
   export let closeIcon: boolean = false;
-  export let variant: 'inline' | 'below' | 'split' = 'inline';
 
   export let classes: {
     root?: string;
@@ -54,6 +57,19 @@
   <div
     class={cls(
       'Notification rounded-lg border bg-surface-100 shadow-lg z-10',
+      {
+        fill: {
+          primary: 'bg-primary text-primary-content',
+          secondary: 'bg-secondary text-secondary-content',
+          accent: 'bg-accent text-accent-content',
+          neutral: 'bg-neutral text-neutral-content',
+          info: 'bg-info text-info-content',
+          success: 'bg-success text-success-content',
+          warning: 'bg-warning text-warning-content',
+          danger: 'bg-danger text-danger-content',
+        }[color],
+        default: {},
+      }[variant],
       classes.root,
       $$props.class
     )}
@@ -69,7 +85,20 @@
           <slot name="icon">
             <Icon
               data={icon}
-              class={cls(variant === 'below' && 'self-start mt-0.5')}
+              class={cls(
+                actionsPlacement === 'below' && 'self-start mt-0.5',
+                variant === 'default' &&
+                  {
+                    primary: 'text-primary',
+                    secondary: 'text-secondary',
+                    accent: 'text-accent',
+                    neutral: 'text-neutral',
+                    info: 'text-info',
+                    success: 'text-success',
+                    warning: 'text-warning',
+                    danger: 'text-danger',
+                  }[color]
+              )}
               classes={classes.icon}
             />
           </slot>
@@ -85,32 +114,71 @@
           {/if}
 
           {#if description || $$slots.description}
-            <div class={cls('text-sm text-surface-content/50', classes.description)}>
+            <div
+              class={cls(
+                'text-sm',
+                {
+                  fill: {
+                    primary: 'text-primary-content/50',
+                    secondary: 'text-secondary-content/50',
+                    accent: 'text-accent-content/50',
+                    neutral: 'text-neutral-content/50',
+                    info: 'text-info-content/50',
+                    success: 'text-success-content/50',
+                    warning: 'text-warning-content/50',
+                    danger: 'text-danger-content/50',
+                  }[color],
+                  default: 'text-surface-content/50',
+                }[variant],
+                classes.description
+              )}
+            >
               <slot name="description">
                 {description}
               </slot>
             </div>
           {/if}
 
-          {#if (actions || $$slots.actions) && variant === 'below'}
+          {#if (actions || $$slots.actions) && actionsPlacement === 'below'}
             <div bind:this={actionsEl} class={cls('mt-2 -ml-4 -mb-2', classes.actions)}>
               <slot name="actions">
                 {#each Object.entries(actions) as [name, fn], i}
-                  <Button color={i === 0 ? 'primary' : 'default'} on:click={() => fn()}
-                    >{name}</Button
+                  <Button
+                    color={i === 0 && variant === 'default' ? 'primary' : 'default'}
+                    class={cls(
+                      i === 0 &&
+                        variant === 'default' &&
+                        {
+                          primary: 'text-primary',
+                          secondary: 'text-secondary',
+                          accent: 'text-accent',
+                          neutral: 'text-neutral',
+                          info: 'text-info',
+                          success: 'text-success',
+                          warning: 'text-warning',
+                          danger: 'text-danger',
+                        }[color]
+                    )}
+                    on:click={() => fn()}
                   >
+                    {name}
+                  </Button>
                 {/each}
               </slot>
             </div>
           {/if}
         </div>
 
-        {#if (actions || $$slots.actions) && variant === 'inline'}
+        {#if (actions || $$slots.actions) && actionsPlacement === 'inline'}
           <div bind:this={actionsEl} class={cls('-my-2 -mr-2', classes.actions)}>
             <slot name="actions">
               {#each Object.entries(actions) as [name, fn], i}
-                <Button color={i === 0 ? 'primary' : 'default'} on:click={() => fn()}>{name}</Button
+                <Button
+                  color={i === 0 && variant === 'default' ? 'primary' : 'default'}
+                  on:click={() => fn()}
                 >
+                  {name}
+                </Button>
               {/each}
             </slot>
           </div>
@@ -120,20 +188,55 @@
           <Button
             icon={mdiClose}
             on:click={() => (open = false)}
-            class="text-surface-content/25 self-start"
+            class={cls(
+              'self-start',
+              {
+                fill: {
+                  primary: 'text-primary-content/25',
+                  secondary: 'text-secondary-content/25',
+                  accent: 'text-accent-content/25',
+                  neutral: 'text-neutral-content/25',
+                  info: 'text-info-content/25',
+                  success: 'text-success-content/25',
+                  warning: 'text-warning-content/25',
+                  danger: 'text-danger-content/25',
+                }[color],
+                default: 'text-surface-content/25',
+              }[variant]
+            )}
           />
         {/if}
       </div>
 
-      {#if (actions || $$slots.actions) && variant === 'split'}
-        <div bind:this={actionsEl} class={cls('grid border-l divide-y', classes.actions)}>
+      {#if (actions || $$slots.actions) && actionsPlacement === 'split'}
+        <div
+          bind:this={actionsEl}
+          class={cls(
+            'grid border-l divide-y',
+            {
+              fill: {
+                primary: 'border-primary-content/25 divide-primary-content/25',
+                secondary: 'border-secondary-content/25 divide-secondary-content/25',
+                accent: 'border-accent-content/25 divide-accent-content/25',
+                neutral: 'border-neutral-content/25 divide-neutral-content/25',
+                info: 'border-info-content/25 divide-info-content/25',
+                success: 'border-success-content/25 divide-success-content/25',
+                warning: 'border-warning-content/25 divide-warning-content/25',
+                danger: 'border-danger-content/25 divide-danger-content/25',
+              }[color],
+              default: 'border-surface-content/25',
+            }[variant],
+            classes.actions
+          )}
+        >
           <slot name="actions">
             {#each Object.entries(actions) as [name, fn], i}
               <Button
-                color={i === 0 ? 'primary' : 'default'}
-                class="rounded-none"
-                on:click={() => fn()}>{name}</Button
+                class={cls('rounded-none', variant === 'default' && i === 0 && 'text-primary')}
+                on:click={() => fn()}
               >
+                {name}
+              </Button>
             {/each}
           </slot>
         </div>
--- packages/svelte-ux/src/routes/docs/components/Notification/+page.svelte
+++ packages/svelte-ux/src/routes/docs/components/Notification/+page.svelte
@@ -1,8 +1,38 @@
 <script lang="ts">
-  import { mdiInbox, mdiCheckCircleOutline } from '@mdi/js';
-
   import { Notification } from 'svelte-ux';
+  import { colors } from '@layerstack/tailwind';
+  import { toTitleCase } from '@layerstack/utils';
+
+  import {
+    mdiInbox,
+    mdiCheckCircleOutline,
+    mdiInformationOutline,
+    mdiAlertOutline,
+    mdiAlertOctagonOutline,
+  } from '@mdi/js';
+
   import Preview from '$lib/components/Preview.svelte';
+
+  function themeColorIcon(color: (typeof colors)[number]) {
+    switch (color) {
+      case 'accent':
+        return mdiInformationOutline;
+      case 'success':
+        return mdiCheckCircleOutline;
+      case 'neutral':
+        return mdiInformationOutline;
+      case 'danger':
+        return mdiAlertOctagonOutline;
+      case 'primary':
+        return mdiInformationOutline;
+      case 'secondary':
+        return mdiInformationOutline;
+      case 'info':
+        return mdiInformationOutline;
+      case 'warning':
+        return mdiAlertOutline;
+    }
+  }
 </script>
 
 <h1>Examples</h1>
@@ -34,7 +64,7 @@
     <Notification
       title="Successfully Saved!"
       icon={mdiCheckCircleOutline}
-      classes={{ icon: { root: 'text-success' } }}
+      color="success"
       closeIcon
     />
   </div>
@@ -48,7 +78,7 @@
       title="Successfully Saved!"
       description="Anyone with a link can now view this file."
       icon={mdiCheckCircleOutline}
-      classes={{ icon: { root: 'text-success' } }}
+      color="success"
       closeIcon
     />
   </div>
@@ -71,7 +101,7 @@
       description="Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur."
       icon={mdiInbox}
       actions={{ Undo: () => alert('Undo'), Dismiss: () => {} }}
-      variant="below"
+      actionsPlacement="below"
       closeIcon
     />
   </div>
@@ -84,9 +114,95 @@
     <Notification
       title="Receive notifications"
       description="Notifications may include alerts, sounds, and badges"
-      actions={{ Reply: () => alert('Allow'), "Don't Allow": () => alert("Don't Allow") }}
-      variant="split"
+      actions={{ Allow: () => alert('Allow'), "Don't Allow": () => alert("Don't Allow") }}
+      actionsPlacement="split"
       classes={{ actions: 'w-40' }}
     />
+  </div>
+</Preview>
+
+<h2>Color</h2>
+
+<Preview>
+  <div class="grid gap-2 w-[400px]">
+    {#each colors as color}
+      <Notification
+        title={toTitleCase(color)}
+        description="An example using {color} color"
+        icon={themeColorIcon(color)}
+        {color}
+        closeIcon
+      />
+    {/each}
+  </div>
+</Preview>
+
+<h2>Variant (fill)</h2>
+
+<Preview>
+  <div class="grid gap-2 w-[400px]">
+    {#each colors as color}
+      <Notification
+        title={toTitleCase(color)}
+        description="An example using {color} color"
+        icon={themeColorIcon(color)}
+        {color}
+        variant="fill"
+        closeIcon
+      />
+    {/each}
+  </div>
+</Preview>
+
+<h2>Variant (fill) with inline actions</h2>
+
+<Preview>
+  <div class="grid gap-2 w-[400px]">
+    {#each colors as color}
+      <Notification
+        title="Example using {color} color"
+        {color}
+        variant="fill"
+        actions={{ Undo: () => alert('Undo') }}
+        closeIcon
+      />
+    {/each}
+  </div>
+</Preview>
+
+<h2>Variant (fill) with actions below</h2>
+
+<Preview>
+  <div class="grid gap-2 w-[400px]">
+    {#each colors as color}
+      <Notification
+        title={toTitleCase(color)}
+        description="An example using {color} color"
+        icon={themeColorIcon(color)}
+        {color}
+        variant="fill"
+        actions={{ Allow: () => alert('Allow'), "Don't Allow": () => alert("Don't Allow") }}
+        actionsPlacement="below"
+      />
+    {/each}
+  </div>
+</Preview>
+
+<h2>Variant (fill) with split actions</h2>
+
+<Preview>
+  <div class="grid gap-2 w-[600px]">
+    {#each colors as color}
+      <Notification
+        title={toTitleCase(color)}
+        description="An example using {color} color"
+        icon={themeColorIcon(color)}
+        {color}
+        variant="fill"
+        actions={{ Allow: () => alert('Allow'), "Don't Allow": () => alert("Don't Allow") }}
+        actionsPlacement="split"
+        classes={{ actions: 'w-40' }}
+      />
+    {/each}
   </div>
 </Preview>
