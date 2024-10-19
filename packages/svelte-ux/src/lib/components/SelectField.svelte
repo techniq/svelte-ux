00dffc0b8cd78e45f70962f0257ed0d14c739f3b
Sean Lynch <techniq35@gmail.com>
2024-10-19 19:01:39 -0400

    Ignore warning

--- packages/svelte-ux/src/lib/components/SelectField.svelte
+++ packages/svelte-ux/src/lib/components/SelectField.svelte
@@ -413,6 +413,7 @@
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
 <div
   aria-haspopup={!inlineOptions ? 'listbox' : undefined}
   class={cls(
