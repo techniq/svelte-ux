75cd1ca4cfe15d07987ba3b403d6c00b7f5a58be
0xmiki <0xmik@proton.me>
2025-06-16 21:42:46 +0300

    tell user to manually install tailwind v3

--- packages/svelte-ux/src/routes/+page.svelte
+++ packages/svelte-ux/src/routes/+page.svelte
@@ -1,54 +1,24 @@
 <script lang="ts">
-  import { Kbd, ToggleGroup, ToggleOption } from 'svelte-ux';
-
+  import { Kbd } from 'svelte-ux';
   import Code from '$lib/components/Code.svelte';
-
-  let selectedTab = 'svelte-cli';
 </script>
 
 <div class="prose max-w-none bg-surface-100 rounded border p-4 mt-4 m-2">
   <h1>Getting started</h1>
 
   <h2>Installation</h2>
-  <ToggleGroup
-    bind:value={selectedTab}
-    variant="underline"
-    classes={{ options: 'justify-start h-10 mb-3' }}
-  >
-    <ToggleOption value="svelte-cli">Svelte CLI</ToggleOption>
-    <ToggleOption value="manual">Manual Install</ToggleOption>
-  </ToggleGroup>
-
   <div class="grid gap-3">
-    {#if selectedTab === 'svelte-cli'}
-      <div>
-        Start a new project SvelteKit project with <a
-          href="https://svelte.dev/docs/cli/overview"
-          target="_blank">sv</a
-        > cli
-      </div>
-      <Code source={`npm sv create`} language="sh" />
+    <div>
+      Install <b>Tailwind v3</b> by following the <a href="https://v3.tailwindcss.com/docs/guides/sveltekit" target="_blank">official guide</a>.
+      <br />
+      (SvelteKit installs Tailwind v4 by default. For v4 support, see <a href="https://next.svelte-ux.techniq.dev/" target="_blank">here</a>.)
+    </div>
+    <div>Add Svelte UX package</div>
+    <Code source={`npm install svelte-ux`} language="sh" />
 
-      <div>Select Tailwind during setup OR run</div>
-      <Code source={`npx sv add tailwindcss`} language="sh" />
-
-      <div>Add Svelte UX package</div>
-      <Code source={`npm install svelte-ux`} language="sh" />
-    {:else if selectedTab === 'manual'}
-      <div>
-        Follow the Tailwind <a href="https://tailwindcss.com/docs/guides/sveltekit" target="_blank">
-          guide
-        </a> to setup a new SvelteKit project with Tailwind.
-      </div>
-
-      <div>Add Svelte UX package</div>
-      <Code source={`npm install svelte-ux`} language="sh" />
-    {/if}
-
-    {#if selectedTab === 'svelte-cli' || selectedTab === 'manual'}
-      <div>Update <code>tailwind.config.cjs</code></div>
-      <Code
-        source={`const colors = require('tailwindcss/colors');
+    <div>Update <code>tailwind.config.cjs</code></div>
+    <Code
+      source={`const colors = require('tailwindcss/colors');
 const layerstack = require('@layerstack/tailwind/plugin');
 
 module.exports = {
@@ -87,13 +57,11 @@ module.exports = {
     layerstack,  // uses hsl() color space by default. To use oklch(), use: layerstack({ colorSpace: 'oklch' }),
   ]
 };`}
-        language="js"
-      />
-    {/if}
+      language="js"
+    />
   </div>
 
   <p>A few notes regarding the <code>tailwind.config.cjs</code></p>
-
   <ul>
     <li>
       <code>{`./node_modules/svelte-ux/**/*.{(svelte, js)}`}</code> adds the library classes via
