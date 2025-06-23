4acb45acb03db877559eceff62812434c49625b2
Sean Lynch <techniq35@gmail.com>
2025-06-23 14:24:41 -0400

    feat(TreeList): Support passing `props` to underlying ul/li elements. Add `data-level` attribute to li for CSS targeting

---
+++ .changeset/violet-books-sneeze.md
@@ -0,0 +1,5 @@
+---
+'svelte-ux': patch
+---
+
+feat(TreeList): Support passing `props` to underlying ul/li elements. Add `data-level` attribute to li for CSS targeting
--- packages/svelte-ux/src/lib/components/TreeList.svelte
+++ packages/svelte-ux/src/lib/components/TreeList.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
   import type { TreeNode } from '@layerstack/utils/array';
   import { cls } from '@layerstack/tailwind';
+
   import { getComponentClasses } from './theme.js';
 
   export let nodes: TreeNode[];
+
+  export let props: {
+    ul?:
+      | HTMLAttributes<HTMLUListElement>
+      | ((nodes: TreeNode[]) => HTMLAttributes<HTMLUListElement>);
+    li?: HTMLAttributes<HTMLLIElement> | ((node: TreeNode) => HTMLAttributes<HTMLLIElement>);
+  } = {};
 
   export let classes: {
     ul?: string | ((nodes: TreeNode[]) => string);
@@ -13,6 +22,7 @@
 </script>
 
 <ul
+  {...typeof props.ul === 'function' ? props.ul(nodes) : props.ul}
   class={cls(
     'TreeList',
     typeof settingsClasses.ul === 'string' ? settingsClasses.ul : settingsClasses.ul?.(nodes),
@@ -22,14 +32,16 @@
 >
   {#each nodes ?? [] as node}
     <li
+      data-level={node.level}
       class={cls(
         typeof settingsClasses.li === 'string' ? settingsClasses.li : settingsClasses.li?.(node),
         typeof classes.li === 'string' ? classes.li : classes.li?.(node)
       )}
+      {...typeof props.li === 'function' ? props.li(node) : props.li}
     >
       <slot {node} />
       {#if node.children}
-        <svelte:self nodes={node.children} {classes} let:node>
+        <svelte:self nodes={node.children} {classes} {props} let:node>
           <slot {node} />
         </svelte:self>
       {/if}
