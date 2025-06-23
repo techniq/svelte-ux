cbca985a7941bd0295c13683c4f9ac4c1a17dfb1
Sean Lynch <techniq35@gmail.com>
2025-06-23 14:23:06 -0400

    docs: Setup unplugin-icons with lucide

--- packages/svelte-ux/src/app.d.ts
+++ packages/svelte-ux/src/app.d.ts
@@ -1,3 +1,5 @@
+// export * from 'unplugin-icons/types/svelte';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
--- packages/svelte-ux/vite.config.js
+++ packages/svelte-ux/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import Icons from 'unplugin-icons/vite';
 import { sveld } from './src/lib/plugins/vite.js';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit() /*, sveld()*/],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    Icons({
+      compiler: 'svelte',
+    }) /*, sveld()*/,
+  ],
   resolve: {
     noExternal: true, // https://github.com/AdrianGonz97/refined-cf-pages-action/issues/26#issuecomment-2878397440
   },
