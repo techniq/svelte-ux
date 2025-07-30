9035a42f0183a1a116b28c84c5026e1d84192582
Sean Lynch <techniq35@gmail.com>
2025-07-30 12:56:35 -0400

    fix(Icon): Restore adding `inline-block` by default

--- packages/svelte-ux/src/lib/components/Icon.svelte
+++ packages/svelte-ux/src/lib/components/Icon.svelte
@@ -90,7 +90,7 @@
   <Icon
     class={cls(
       'Icon',
-      // 'icon-container _inline-block shrink-0 _align-middle _fill-current',
+      'icon-container inline-block shrink-0 align-middle',
       'size-[1.2em]', // default scale of unplugin-icons - https://github.com/unplugin/unplugin-icons?tab=readme-ov-file#options
       settingsClasses.root,
       classes.root,
